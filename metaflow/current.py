15a7b1c0d056014184e6d67d4afd09e4aa637e7c
savin <savin@netflix.com>
2020-04-21 18:10:04 -0700

    Expose Retry Count in Current Singleton
    current.retry_count now returns the retry attempt (0, 1, .. so on)
    for a metaflow task

--- metaflow/current.py
+++ metaflow/current.py
@@ -6,28 +6,42 @@ class Current(object):
         self._run_id = None
         self._step_name = None
         self._task_id = None
+        self._retry_count = None
         self._origin_run_id = None
         self._namespace = None
         self._username = None
         self._is_running = False
 
     def _set_env(self,
-                 flow_name,
-                 run_id,
-                 step_name,
-                 task_id,
-                 origin_run_id,
-                 namespace,
-                 username):
+                 flow_name=None,
+                 run_id=None,
+                 step_name=None,
+                 task_id=None,
+                 retry_count=None,
+                 origin_run_id=None,
+                 namespace=None,
+                 username=None,
+                 is_running=True):
 
         self._flow_name = flow_name
         self._run_id = run_id
         self._step_name = step_name
         self._task_id = task_id
+        self._retry_count = retry_count
         self._origin_run_id = origin_run_id
         self._namespace = namespace
         self._username = username
-        self._is_running = True
+        self._is_running = is_running
+
+    def _update_env(self, env):
+        for k, v in env.items():
+            setattr(self.__class__, k, property(fget=lambda _, v=v: v))
+
+    def __contains__(self, key):
+        return getattr(self, key, None) is not None
+
+    def get(self, key, default=None):
+        return getattr(self, key, default)
 
     def _update_env(self, env):
         for k, v in env.items():
@@ -52,6 +66,10 @@ class Current(object):
     @property
     def task_id(self):
         return self._task_id
+
+    @property
+    def retry_count(self):
+        return self._retry_count
 
     @property
     def origin_run_id(self):
