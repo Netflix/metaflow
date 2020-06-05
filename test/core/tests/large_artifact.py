473987ef5ea50294dafbbda1a7bf81840b352ab6
savin <savin@netflix.com>
2020-06-04 18:40:18 -0700

    Disable LargeArtifactTest on GitHub CI

--- test/core/tests/large_artifact.py
+++ test/core/tests/large_artifact.py
@@ -2,7 +2,7 @@ from metaflow_test import MetaflowTest, ExpectationFailed, steps
 
 class LargeArtifactTest(MetaflowTest):
     """
-    Test that you can serialize large objects (over 2GB)
+    Test that you can serialize large objects (over 4GB)
     with Python3.
     """
     PRIORITY = 2
@@ -11,7 +11,7 @@ class LargeArtifactTest(MetaflowTest):
     def step_single(self):
         import sys
         if sys.version_info[0] > 2:
-            self.large = b'x' * int(2.1 * 1024**3)
+            self.large = b'x' * int(4.1 * 1024**3)
             self.noop = False
         else:
             self.noop = True
@@ -20,7 +20,7 @@ class LargeArtifactTest(MetaflowTest):
     def step_end(self):
         import sys
         if sys.version_info[0] > 2:
-            assert_equals(self.large, b'x' * int(2.1 * 1024**3))
+            assert_equals(self.large, b'x' * int(4.1 * 1024**3))
 
     @steps(1, ['all'])
     def step_all(self):
@@ -32,4 +32,4 @@ class LargeArtifactTest(MetaflowTest):
         if not noop and sys.version_info[0] > 2:
             checker.assert_artifact('end',
                                     'large',
-                                    b'x' * int(2.1 * 1024**3))
+                                    b'x' * int(4.1 * 1024**3))
