cdd16aee81c7ccedd27b864c58b08fdae3acc4dc
Sreehari Sreejith <sreeharis@zillowgroup.com>
2020-08-03 12:10:04 -0400

    Remove unused code. Minor refactor.

--- metaflow/plugins/kfp/kfp.py
+++ metaflow/plugins/kfp/kfp.py
@@ -3,6 +3,7 @@ from kfp import dsl
 
 from .constants import DEFAULT_FLOW_CODE_URL, DEFAULT_KFP_YAML_OUTPUT_PATH
 from typing import NamedTuple
+from collections import deque
 
 def step_op_func(python_cmd_template, step_name: str,
                  code_url: str,
@@ -141,37 +142,41 @@ def pre_start_container_op():
     pre_start_op = kfp.components.func_to_container_op(pre_start_op_func, base_image='ssreejith3/mf_on_kfp:python-curl-git')
     return pre_start_op
 
-def create_flow_from_graph(flowgraph, flow_code_url=DEFAULT_FLOW_CODE_URL):
-    from collections import deque
-    graph = flowgraph
-    code_url = flow_code_url
+def create_command_templates_from_graph(graph):
+    """
+    Create a map of steps to their corresponding command templates. These command templates help define the command
+    to be used to run that particular step with placeholders for the `run_id` and `datastore_root` (location of the datastore).
 
+    # Note:
+    # Level-order traversal is adopted to keep the task-ids in line with what happens during a local metaflow execution.
+    # It is not entirely necessary to keep this order of task-ids if we are able to point to the correct input-paths for
+    # each step. But, using this ordering does keep the organization of data more understandable and natural (i.e., `start`
+    # gets a task id of 1, next step gets a task id of 2 and so on with 'end' step having the highest task id.
+    """
     def build_cmd_template(step_name, task_id, input_paths):
         python_cmd = "python helloworld.py --datastore s3 --datastore-root {{ds_root}} step {step_name} --run-id {{run_id}} --task-id {task_id} --input-paths {input_paths}".format(step_name=step_name, task_id=task_id, input_paths=input_paths )
         return python_cmd
 
-    # Note: Below, we process the graph in level order and save the command templates to be used for each step of the graph.
-    # Level-order traversal is adopted to keep the task-ids in line with what happens during a local metaflow execution.
-    # It may not be necessary to keep this order of task-ids (TODO: verify importance of task-id ordering) as long as we
-    # are able to point to the correct input-paths for each step.
-
-    dq = deque(['start']) # deque to process the DAG in level order
+    steps_deque = deque(['start']) # deque to process the DAG in level order
     cur_task_id = 0
-    cur_input_path = '' # non-join node: run-id/parent-step/parent-task-id, branch-join: run-id/:p1/p1-task-id,p2/p2-task-id,...
 
     # set of seen steps, i.e., added to the queue for processing
-    seen = set(['start'])
-
+    seen_steps = set(['start'])
     # Mapping of steps to task ids
     task_id_map = {}
     # Mapping of steps to their command templates
     command_template_map = {}
 
-    while len(dq) > 0:
-        cur_step = dq.popleft()
+    while len(steps_deque) > 0:
+        cur_step = steps_deque.popleft()
         cur_task_id += 1
         task_id_map[cur_step] = cur_task_id
 
+        # Generate the correct input_path for each step. Note: input path depends on a step's parents (i.e., in_funcs)
+        # Format of the input-paths for reference:
+        # non-join nodes: run-id/parent-step/parent-task-id,
+        # branch-join node: run-id/:p1/p1-task-id,p2/p2-task-id,...
+        # foreach node: TODO: foreach is not considered here
         if cur_task_id == 1: # start step
             cur_input_path = '{run_id}/_parameters/0' # this is the standard input path for the `start` step
         else:
@@ -188,13 +193,24 @@ def create_flow_from_graph(flowgraph, flow_code_url=DEFAULT_FLOW_CODE_URL):
 
         out_funcs = graph.nodes[cur_step].out_funcs
         for step in out_funcs:
-            if step not in seen:
-                dq.append(step)
-                seen.add(step)
+            if step not in seen_steps:
+                steps_deque.append(step)
+                seen_steps.add(step)
+
+    return command_template_map
+
+
+def create_flow_from_graph(flowgraph, flow_code_url=DEFAULT_FLOW_CODE_URL):
+
+    graph = flowgraph
+    code_url = flow_code_url
+
+    command_template_map = create_command_templates_from_graph(graph)
 
     @dsl.pipeline(
         name='MF on KFP Pipeline',
-        description='Pipeline defining KFP equivalent of the Metaflow flow'
+        description='Pipeline defining KFP equivalent of the Metaflow flow. Currently supports linear flows and flows '
+                    'with branch and join nodes'
     )
     def kfp_pipeline_from_flow():
         pre_start_op = (pre_start_container_op())(code_url)
@@ -204,7 +220,7 @@ def create_flow_from_graph(flowgraph, flow_code_url=DEFAULT_FLOW_CODE_URL):
         ds_root = outputs['ds_root']
         run_id = outputs['run_id']
 
-        # Initial:
+        # Initial setup
         pre_start_op.set_display_name('InitialSetup')
         container_op_map['start'] = (step_container_op())(command_template_map['start'], 'start', code_url, ds_root, run_id)
         container_op_map['start'].set_display_name('start')
@@ -219,94 +235,6 @@ def create_flow_from_graph(flowgraph, flow_code_url=DEFAULT_FLOW_CODE_URL):
 
     return kfp_pipeline_from_flow
 
-def create_flow_pipeline_alternate(mf_graph, flow_code_url=DEFAULT_FLOW_CODE_URL):
-    """
-    Function used to create the KFP flow pipeline. Return the function defining the KFP equivalent of the flow
-    """
-
-    graph = mf_graph
-    code_url = flow_code_url
-    print("\nCreating the pipeline definition needed to run the flow on KFP...\n")
-    print("\nCode URL of the flow to be converted to KFP: {0}\n".format(flow_code_url))
-
-    @dsl.pipeline(
-        name='MF on KFP Pipeline',
-        description='Pipeline defining KFP equivalent of the Metaflow flow'
-    )
-    def kfp_pipeline_from_flow():
-        """
-        This function converts the flow steps to kfp container op equivalents
-        by invoking `step_container_op` for every step in the flow and handling the order of steps.
-        """
-
-        step_container_ops = []
-        pre_start_op = (pre_start_container_op())(code_url)
-        step_container_ops.append(pre_start_op)
-
-        from collections import deque
-        import ast
-        import json
-
-        dq = deque([pre_start_op])
-        seen_in_funcs = set([])
-        input_paths_join = None
-        is_branch_join = True # Ignore foreach for now
-        pre_join_tasks = []
-
-        while len(dq) > 0:
-            prev_task = dq.popleft()
-            prev_step_outputs = prev_task.outputs
-
-            out_funcs = json.loads(prev_step_outputs['next_step'])
-            next_task_ids = json.loads(prev_step_outputs['next_task_id'])
-            out_funcs = ast.literal_eval(str(out_funcs))
-
-            for i, step in enumerate(out_funcs):
-                node_type = graph.nodes[step].type
-                node_in_funcs = graph.nodes[step].in_funcs
-                if len(node_in_funcs) <= 1: # non-join nodes
-                    # create the current task
-                    # no dependencies as this is start step
-                    input_path = prev_step_outputs['ds_root'] + "/" + prev_step_outputs['current_step'] + "/" + prev_step_outputs['current_task_id']  # runid/step/taskid
-                    current_task = (step_container_op())(
-                        step,
-                        code_url,
-                        prev_step_outputs['ds_root'],
-                        prev_step_outputs['run_id'],
-                        next_task_ids[0], # there will only be one as it's the start step
-                        input_path
-                    )
-                    current_task.set_display_name(step)
-                    current_task.after(prev_task)
-                    dq.append(current_task)
-
-                else: # join
-                    if input_paths_join is None and is_branch_join:
-                        input_paths_join = prev_step_outputs['run_id'] + "/:"
-                    seen_in_funcs.add(prev_step_outputs['current_step'])
-                    input_paths_join += prev_step_outputs['current_step'] + "/" + prev_step_outputs['current_task_id']
-                    pre_join_tasks.append(prev_task)
-                    if seen_in_funcs == node_in_funcs: # then the join has all the data it needs to start execution
-                        current_task =  (step_container_op())(
-                                            step,
-                                            code_url,
-                                            prev_step_outputs['ds_root'],
-                                            prev_step_outputs['run_id'],
-                                            next_task_ids[0],  # new task id - check if this is always correct
-                                            input_paths_join
-                                        )
-                        current_task.set_display_name(step)
-                        for pre_join_task in pre_join_tasks:
-                            current_task.after(pre_join_task)
-                        # Reset to prep for the next join
-                        input_paths_join = None
-                        seen_in_funcs = set([])
-                        pre_join_tasks = []
-                    else:
-                        # There's more to come. So, add a comma
-                        input_paths_join += ','
-
-    return kfp_pipeline_from_flow
 
 def create_run_on_kfp(flowgraph, code_url, experiment_name, run_name):
     """
