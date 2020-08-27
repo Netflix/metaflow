397f26bc63fa86f0fb973e2b6ea756595d860542
Bryan Galvin <bryan.galvin@latimes.com>
2020-08-27 06:09:30 -0700

    + new install function
    renamed to install_metaflow() to follow convention and avoid namespace issues with devtools::install()

--- R/R/zzz.R
+++ R/R/zzz.R
@@ -1,22 +1,47 @@
-.onAttach <- function(libname, pkgname) {
-  tryCatch(
-    {
-      metaflow_attach()
-    },
-    error = function(e) {
-      packageStartupMessage("  * Call 'metaflow::install()' to finish installation * ")
-    }
+pkg.env <- new.env()
+
+pkg.env$configs <- list(
+  default = list(
+    metaflow_path = expression(reticulate::py_discover_config("metaflow")$required_module_path)
+  ),
+  batch = list(
+    metaflow_path = expression(path.expand(paste0(getwd(), "/metaflow")))
   )
-  invisible(NULL)
+)
+
+.onAttach <- function(libname, pkgname) {
+  if (py_module_available("metaflow")) {
+    metaflow_attach()
+  }
 }
 
 .onLoad <- function(libname, pkgname) {
-  tryCatch(
-    {
-      metaflow_load()
-    },
-    error = function(e) {
-    }
-  )
+  metaflow_python <- Sys.getenv("METAFLOW_PYTHON", unset = NA)
+  if (!is.na(metaflow_python))
+    Sys.setenv(RETICULATE_PYTHON = metaflow_python)
+  
+  if (!ensure_metaflow()) {
+    packageStartupMessage("* Metaflow Python dependencies not found *\n",
+                          "    - Call `install_metaflow()` to install dependencies into a new conda or virtualenv\n",
+                          "    - Set `METAFLOW_PYTHON` environment variable to point to a custom metaflow installation.")
+  } else {
+    metaflow_load()
+  }
+  
+}
+
+metaflow_load <- function() {
+  config_name <- Sys.getenv("R_CONFIG_ACTIVE", unset = "default")
+  configs <- pkg.env$configs
+  config <- list()
+  for (key in names(configs[[config_name]])) {
+    config[[key]] <- eval(configs[[config_name]][[key]])
+  }
+  if (config_name == "batch") {
+    pkg.env$mf <- import_from_path("metaflow", path = config$metaflow_path)
+  } else {
+    pkg.env$mf <- import("metaflow", delay_load = TRUE)
+  }
   invisible(NULL)
 }
+
