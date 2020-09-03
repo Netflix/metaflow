ebd4f3a1fce446eba066b767d371c873d2ef2709
Bryan Galvin <bryan.galvin@latimes.com>
2020-09-02 21:01:58 -0700

    import metaflow R in testthat

--- R/tests/testthat.R
+++ R/tests/testthat.R
@@ -1,8 +1,4 @@
 library(testthat)
-library(reticulate)
-
-virtualenv_create("metaflow-r")
-virtualenv_install("metaflow-r", "../..")
-use_virtualenv("metaflow-r")
+library(metaflow)
 
 test_check("metaflow")
