563fb8606109d6df48ac71bd7151d9eab927a1ca
Jason Ge <jge@netflix.com>
2020-10-26 11:48:04 -0700

    fixing a bug in R CI setup

--- R/tests/run_tests.R
+++ R/tests/run_tests.R
@@ -2,12 +2,7 @@ library(reticulate)
 
 virtualenv_create("r-metaflow")
 virtualenv_install("r-metaflow", c("../..", "pandas", "numpy"))
-print('finish venv install')
 use_virtualenv("r-metaflow")
-print("using r-metaflow")
-#python_bin <- py_discover_config("metaflow", "r-metaflow")$python
-#print(python_bin)
-#Sys.setenv("METAFLOW_PYTHON" = python_bin)
 
 source("testthat.R")
 source("run_integration_tests.R")
