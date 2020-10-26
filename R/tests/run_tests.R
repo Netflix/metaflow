bf64a4f797ddb14da529baf10d096ab8198cd394
Jason Ge <jge@netflix.com>
2020-10-26 11:33:28 -0700

    debug R ci setup

--- R/tests/run_tests.R
+++ R/tests/run_tests.R
@@ -2,7 +2,9 @@ library(reticulate)
 
 virtualenv_create("r-metaflow")
 virtualenv_install("r-metaflow", c("../..", "pandas", "numpy"))
+print('finish venv install')
 use_virtualenv("r-metaflow")
+print("using r-metaflow")
 python_bin <- py_discover_config("metaflow", "r-metaflow")$python
 print(python_bin)
 Sys.setenv("METAFLOW_PYTHON" = python_bin)
