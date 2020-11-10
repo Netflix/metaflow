9af8a185d191519db093ae657c1d1f4a9688b926
Taleb Zeghmi <talebz@zillowgroup.com>
2020-11-10 11:18:27 -0800

    Sync upstream merge fixes

--- metaflow/plugins/kfp/kfp_decorator.py
+++ metaflow/plugins/kfp/kfp_decorator.py
@@ -40,7 +40,9 @@ class KfpInternalDecorator(StepDecorator):
         """
         # TODO: any other KFP environment variables to get and register to Metadata service?
         meta = {"kfp-execution": run_id}
-        entries = [MetaDatum(field=k, value=v, type=k) for k, v in meta.items()]
+        entries = [
+            MetaDatum(field=k, value=v, type=k, tags=[]) for k, v in meta.items()
+        ]
         # Register book-keeping metadata for debugging.
         metadata.register_metadata(run_id, step_name, task_id, entries)
 
