384a3ae7c8d5ae2333eda1f64d18fbb1ddb8719e
Vinai Rachakonda <rachakondavinai@gmail.com>
2020-12-15 15:59:07 -0500

    Add the demo code

---
+++ dolt_demo.py
@@ -0,0 +1,32 @@
+from metaflow import FlowSpec, step, DoltDT
+import json
+import numpy as np
+
+class DoltDemoFlow(FlowSpec):
+
+    @step
+    def start(self):
+        import pandas
+        self.df = pandas.read_csv('movies.csv')
+
+        self.next(self.add_random)
+
+    # Add a random number to the gross column.
+    @step
+    def add_random(self):
+        import random
+
+        with DoltDT(run=self, repoOwner='vinai', repoName='metaflow_demo') as dolt:
+            self.df['gross'] = self.df['gross'] + random.randint(1, 1000000)
+
+            dolt.add_table(table_name='movies', df=self.df, pks=['movie_title'])
+
+        self.next(self.end)
+
+    @step
+    def end(self):
+        with DoltDT(run=self, repoOwner='vinai', repoName='metaflow_demo') as dolt:
+            dolt.commit_and_push()
+
+if __name__ == '__main__':
+    DoltDemoFlow()
