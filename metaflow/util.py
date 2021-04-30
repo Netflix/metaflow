93852058f2415bafbb1510695aa0c8ea592f4cc9
Romain <romain-intel@users.noreply.github.com>
2021-04-29 18:33:13 -0700

    Revert usage of shlex for Python2 (not compatible) (#498)
    Allow quoted values in decospec (due to aforementioned quote)

--- metaflow/util.py
+++ metaflow/util.py
@@ -8,7 +8,6 @@ from functools import wraps
 from io import BytesIO
 from itertools import takewhile
 import re
-import shlex
 
 from metaflow.exception import MetaflowUnknownUser, MetaflowInternalError
 
@@ -39,6 +38,7 @@ try:
         def __str__(self):
             return self.path
 
+    from pipes import quote as _quote
 except:
     # python3
     unicode_type = str
@@ -49,6 +49,7 @@ except:
     def unquote_bytes(x):
         return unquote(to_unicode(x))
 
+    from shlex import quote as _quote
 
 class TempDir(object):
     # Provide a temporary directory since Python 2.7 does not have it inbuilt
@@ -306,7 +307,7 @@ def dict_to_cli_options(params):
                         yield value
                     else:
                         # Otherwise, assume it is a literal value and quote it safely
-                        yield shlex.quote(value)
+                        yield _quote(value)
 
 
 # This function is imported from https://github.com/cookiecutter/whichcraft
