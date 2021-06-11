0158f8bf7ac306fe456fa7ff85ed9b231cfbf2bc
mdneuzerling <david@neuzerling.com>
2021-06-12 09:29:54 +1000

    Roxygen for internal functions

--- R/R/decorators.R
+++ R/R/decorators.R
@@ -28,11 +28,44 @@ new_decorator <- function(x) {
   )
 }
 
+#' Format a list of decorators as a character vector
+#'
+#' @section Python decorators: Metaflow decorators are so called because they
+#'   translate directly to Python decorators that are applied to a step. So, for
+#'   example, `decorator("batch", cpu = 1)` in R becomes `@batch(cpu = 1)` in
+#'   Python. A new line is appended as well, as Python decorators are placed
+#'   above the function they take as an input.
+#'
+#' @param decorators List of decorators, as created by the
+#'   \code{\link{decorators}} function.
+#'
+#' @return character vector
+#' @keywords internal
+#' 
+#' @examples \dontrun{
+#' add_decorators(list(decorator("batch", cpu = 4), decorator("retry")))
+#' #> c("@batch(cpu=4)", "\n", "@retry", "\n")
+#' }
 add_decorators <- function(decorators) {
   decorator_idx <- unlist(lapply(decorators, is.decorator))
   unlist(decorators[decorator_idx])
 }
 
+#' Format an R decorator as a Python decorator
+#' 
+#' @inheritSection add_decorators Python decorators
+#'
+#' @param x Decorator name.
+#' @param ... Additional named arguments provided to the decorator
+#'
+#' @return character vector of length two, in which the first element is the 
+#' translated decorator and the second element is a new line character.
+#' @keywords internal
+#'
+#' @examples \dontrun{
+#' fmt_decorator("resources", cpu = 1, memory = 1000)
+#' # returns c("@resources(cpu=1, memory=1000)", "\n")
+#' }
 fmt_decorator <- function(x, ...) {
   args <- decorator_arguments(list(...))
   decorator_string <- paste0("@", x)
@@ -44,6 +77,20 @@ fmt_decorator <- function(x, ...) {
   c(decorator_string, "\n")
 }
 
+#' Format the arguments of a decorator as inputs to a Python function
+#'
+#' @inheritSection add_decorators Python decorators
+#'
+#' @param args Named list of arguments, as would be provided to the `...` of a
+#'   function.
+#'
+#' @return atomic character of arguments, separated by a comma
+#' @keywords internal
+#'
+#' @examples \dontrun{
+#' decorator_arguments(list(cpu = 1, memory = 1000))
+#' #> "cpu=1, memory=1000"
+#' }
 decorator_arguments <- function(args) {
   argument_names <- names(args)
   if (any(duplicated(argument_names))) {
