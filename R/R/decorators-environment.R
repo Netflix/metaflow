f321a985896a471cb67af01b9596778219a3f042
mdneuzerling <david@neuzerling.com>
2021-06-12 16:19:36 +1000

    Docstrings for environment_variables

--- R/R/decorators-environment.R
+++ R/R/decorators-environment.R
@@ -1,3 +1,27 @@
+#' Decorator that sets environment variables during step execution
+#'
+#' @param ... Named environment variables and their values. For example, 
+#'   `environment_variables(foo = "bar")` will set the "foo" environment 
+#'   variable as "bar" during step execution.
+#'
+#' @inherit decorator return
+#' 
+#' @export
+#'
+#' @examples \dontrun{
+#' start <- function(self) {
+#'   print(paste("The cutest animal is the", Sys.getenv("CUTEST_ANIMAL")))
+#'   print(paste("The", Sys.getenv("ALSO_CUTE"), "is also cute, though"))
+#' }
+#' 
+#' metaflow("EnvironmentVariables") %>%
+#'   step(step="start", 
+#'        environment_variables(CUTEST_ANIMAL = "corgi", ALSO_CUTE = "penguin"),
+#'        r_function=start, 
+#'        next_step="end") %>%
+#'   step(step="end") %>% 
+#'   run()
+#' }
 environment_variables <- function(...) {
   env_vars <- list(...)
   if (length(env_vars) == 0) {
