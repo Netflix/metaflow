6b256a7f101fc955165d9befbece9d7e30a6f7d0
Ryan Williams <ryan@runsascoded.com>
2021-06-18 21:30:50 -0400

    api: `_mf` meta constants

--- metaflow/meta.py
+++ metaflow/meta.py
@@ -1,6 +1,12 @@
 """Constants associated with storing step/graph metadata on step functions"""
 
 META_KEY = "_mf"
+FOREACH = "foreach"
+IFF = "iff"
+IFN = "ifn"
+JOIN = "join"
 IS_STEP = "is_step"
+ORIG_FN = "orig_fn"
+PREV_STEP = "prev_step"
 
 FLOWSPEC_LOAD_FILE = "_flowspec_load_file"
