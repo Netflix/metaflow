999ad1e01f0a6e92fd35522cc0bcf1ad5d90488c
Vama Mehta <vama.mehta@inqubit.in>
2021-10-06 11:41:22 +0530

    fix: Removed forward slash to prevent double slashes on DATATOOLS SUFFIX #684

--- metaflow/metaflow_config.py
+++ metaflow/metaflow_config.py
@@ -60,7 +60,7 @@ DATASTORE_SYSROOT_LOCAL = from_conf('METAFLOW_DATASTORE_SYSROOT_LOCAL')
 # S3 bucket and prefix to store artifacts for 's3' datastore.
 DATASTORE_SYSROOT_S3 = from_conf('METAFLOW_DATASTORE_SYSROOT_S3')
 # S3 datatools root location
-DATATOOLS_SUFFIX = from_conf('METAFLOW_DATATOOLS_SUFFIX', 'data')
+DATATOOLS_SUFFIX = from_conf('METAFLOW_DATATOOLS_SUFFIX', 'data').rstrip('/')
 DATATOOLS_S3ROOT = from_conf(
     'METAFLOW_DATATOOLS_S3ROOT', 
         '%s/%s' % (from_conf('METAFLOW_DATASTORE_SYSROOT_S3').rstrip('/'), DATATOOLS_SUFFIX)
