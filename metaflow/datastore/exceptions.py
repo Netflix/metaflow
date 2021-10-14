f952b10e8e915b1b46018c74bc0ddc4c4a976fdd
Valay Dave <valaygaurang@gmail.com>
2021-10-14 15:57:06 -0700

    handling exceptions for non-picklizable artifacts

--- metaflow/datastore/exceptions.py
+++ metaflow/datastore/exceptions.py
@@ -2,3 +2,11 @@ from ..exception import MetaflowException
 
 class DataException(MetaflowException):
     headline = "Data store error"
+
+
+class UnpicklizableArtifactException(MetaflowException):
+    headline = "Cannot Picklize Artifact"
+
+    def __init__(self,artifact_name):
+        msg = 'Cannot pickle dump artifact named "%s"' % artifact_name
+        super().__init__(msg=msg, lineno=None)
