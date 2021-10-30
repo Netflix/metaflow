51060ecb2a168e43b33681bff42f4e0c549995b7
Ujjwal <ujjwal760@gmail.com>
2021-10-30 13:22:24 +0530

    made changes to mflog.py

--- metaflow/mflog/mflog.py
+++ metaflow/mflog/mflog.py
@@ -9,7 +9,14 @@ from metaflow.util import to_bytes, to_fileobj, to_unicode
 
 VERSION = b"0"
 
-RE = b"(\[!)?" b"\[MFLOG\|" b"(0)\|" b"(.+?)Z\|" b"(.+?)\|" b"(.+?)\]" b"(.*)"
+RE = b'(\\[!)?'\
+     b'\\[MFLOG\\|'\
+     b'(0)\\|'\
+     b'(.+?)Z\\|'\
+     b'(.+?)\\|'\
+     b'(.+?)\\]'\
+     b'(.*)' 
+
 
 # the RE groups defined above must match the MFLogline fields below
 # except utc_timestamp, which is filled in by the parser based on utc_tstamp_str
