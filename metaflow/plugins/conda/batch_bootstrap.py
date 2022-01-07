33db00d7d2cfd9ae09e4dd0653c8716f7897d089
Oleg Avdeev <oleg.v.avdeev@gmail.com>
2022-01-07 12:03:27 -0800

    make it work without root

--- metaflow/plugins/conda/batch_bootstrap.py
+++ metaflow/plugins/conda/batch_bootstrap.py
@@ -50,7 +50,7 @@ def install_conda_environment(env_id, packages):
     args = [
         "if ! type conda  >/dev/null 2>&1; \
             then wget --no-check-certificate https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh -O Miniforge3.sh >/dev/null 2>&1; \
-            bash ./Miniforge3.sh -b >/dev/null 2>&1; ln -sf /root/miniforge3/bin/conda /usr/local/bin/conda ; fi",
+            bash ./Miniforge3.sh -b >/dev/null 2>&1; export PATH=$PATH:$HOME/miniforge3/bin; fi",
         "cd {0}".format(os.path.join(os.getcwd(), "pkgs")),
         "conda create --yes --no-default-packages -p {0} --no-deps {1} >/dev/null 2>&1".format(
             os.path.join(os.getcwd(), env_id), " ".join(packages)
