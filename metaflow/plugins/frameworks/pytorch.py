00893fe097dc54b6a5a5d3c2b6828fb71ce8d98e
Aapo Kyrola <akyrola@cs.cmu.edu>
2022-01-13 18:33:20 -0800

    address comments for PR

--- metaflow/plugins/frameworks/pytorch.py
+++ metaflow/plugins/frameworks/pytorch.py
@@ -30,10 +30,16 @@ def setup_torch_distributed(master_port=None):
     """
     # Choose port depending on run id to reduce probability of collisions, unless
     # provided by the user.
-    master_port = master_port or (51000 + abs(int(current.run_id)) % 10000)
+    try:
+        master_port = master_port or (51000 + abs(int(current.run_id)) % 10000)
+    except:
+        # if int() fails, i.e run_id is not an int use just a constant port. Can't use hash()
+        # as that is not constant.
+        master_port = 51001
     os.environ["MASTER_PORT"] = str(master_port)
     os.environ["MASTER_ADDR"] = current.parallel.main_ip
     os.environ["NODE_RANK"] = str(current.parallel.node_index)
     os.environ["WORLD_SIZE"] = str(current.parallel.num_nodes)
     os.environ["NUM_NODES"] = str(current.parallel.num_nodes)
+    # Specific for PyTorch Lightning
     os.environ["PL_TORCH_DISTRIBUTED_BACKEND"] = "gloo"  # NCCL crashes on aws batch!
