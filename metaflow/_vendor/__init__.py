84c2b8d557f1af68de4c6c573e7777708f796b4c
savin <savingoyal@gmail.com>
2022-01-25 13:13:00 -0800

    v

---
+++ metaflow/_vendor/__init__.py
@@ -0,0 +1,8 @@
+"""
+metaflow._vendor is for vendoring dependencies of metaflow. Files 
+inside of metaflow._vendor should be considered immutable and 
+should only be updated to versions from upstream. 
+
+If you would like to debundle the vendored dependencies, please 
+reach out to the maintainers at chat.metaflow.org
+"""
