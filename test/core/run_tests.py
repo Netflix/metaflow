d06b79b4c9f922b40f9134cdd56878357e73cf9e
Valay Dave <valaygaurang@gmail.com>
2022-02-09 09:02:32 -0800

    fix

--- test/core/run_tests.py
+++ test/core/run_tests.py
@@ -102,8 +102,8 @@ def run_test(formatter, context, debug, checks, env_base):
         pythonpath = os.environ.get("PYTHONPATH", ".")
         env.update(
             {
-                "LANG": "C.UTF-8",
-                "LC_ALL": "C.UTF-8",
+                "LANG": "en_US.UTF-8",
+                "LC_ALL": "en_US.UTF-8",
                 "PATH": os.environ.get("PATH", "."),
                 "PYTHONIOENCODING": "utf_8",
                 "PYTHONPATH": "%s:%s" % (package, pythonpath),
