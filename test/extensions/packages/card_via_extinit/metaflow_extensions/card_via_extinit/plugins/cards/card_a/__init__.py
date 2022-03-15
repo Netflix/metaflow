8d047898d889b157d5e0475d0cf0c6310c16d945
Valay Dave <valaygaurang@gmail.com>
2022-03-15 15:34:50 -0700

    Tests for Extensions  (#978)
    * Added tests for extensions. Checking if they work.
    
    * dummy commit to see if things work.
    
    * fix
    
    * debug
    
    * bug fix.
    
    * possible fix.
    
    * tweeking context
    
    * Bug fix to tests.
    
    * dummy commit
    
    * bug fix
    
    * Added extension test to core tests.
    - remove seperate gh action
    
    * removing files.
    
    * fix
    
    * added extension test to py3 context
    - remove redundant complexity.

---
+++ test/extensions/packages/card_via_extinit/metaflow_extensions/card_via_extinit/plugins/cards/card_a/__init__.py
@@ -0,0 +1,15 @@
+from metaflow.cards import MetaflowCard
+
+
+class TestMockCard(MetaflowCard):
+    type = "card_ext_init_a"
+
+    def __init__(self, options={"key": "task"}, **kwargs):
+        self._key = options["key"] if "key" in options else "task"
+
+    def render(self, task):
+        task_data = task[self._key].data
+        return "%s" % task_data
+
+
+CARDS = [TestMockCard]
---
+++ test/extensions/packages/card_via_extinit/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_namespace_packages, setup
+
+
+def get_long_description() -> str:
+    with open("README.md") as fh:
+        return fh.read()
+
+
+setup(
+    name="metaflow-card-via-extinit",
+    version="1.0.0",
+    description="A desription of your card",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    author="Your Name",
+    author_email="your_name@yourdomain.com",
+    license="Apache Software License 2.0",
+    packages=find_namespace_packages(include=["metaflow_extensions.*"]),
+    include_package_data=True,
+    zip_safe=False,
+)
