efe257629ad8b79589926fbc2987db3e11dadaa0
Brendan Gibson <brendan@outerbounds.co>
2022-08-02 16:04:01 +1000

    Add postMessage with content height

--- metaflow/plugins/cards/ui/src/App.svelte
+++ metaflow/plugins/cards/ui/src/App.svelte
@@ -3,7 +3,7 @@
   import "./prism";
   import "./global.css";
   import "./prism.css";
-  import "./app.css"
+  import "./app.css";
   import { cardData, setCardData, modal } from "./store";
   import * as utils from "./utils";
   import Aside from "./components/aside.svelte";
@@ -11,19 +11,56 @@
   import Main from "./components/main.svelte";
   import Modal from "./components/modal.svelte";
   import Nav from "./components/aside-nav.svelte";
+  import { onMount } from "svelte";
 
   export let cardDataId: string;
 
+  let el: HTMLElement;
+  const POST_MESSAGE_RESIZE_TYPE = "PluginHeightCheck";
+
   // Get the data from the element in `windows.__MF_DATA__` corresponding to `cardDataId`. This allows multiple sets of
   // data to exist on a single page
-  setCardData(cardDataId)
+  setCardData(cardDataId);
 
   // Set the `embed` class to hide the `aside` if specified in the URL
   const urlParams = new URLSearchParams(window?.location.search);
-  let embed = Boolean(urlParams.get('embed'))
+  let embed = Boolean(urlParams.get("embed"));
+
+  onMount(() => {
+    // due to issue: https://github.com/Netflix/metaflow-ui/issues/74,
+    // we need to tell the parent in a potential cross-domain iframe that the app div has changed.
+    window?.parent.postMessage(
+      {
+        type: POST_MESSAGE_RESIZE_TYPE,
+        height: Math.max(
+          el?.scrollHeight ?? 0,
+          el?.clientHeight ?? 0,
+          el?.offsetHeight ?? 0
+        ),
+      },
+      "*"
+    );
+
+    // lets continue to update the postMessage if the component changes size.
+    try {
+      const observer = new ResizeObserver(([entry]) => {
+        window?.parent.postMessage(
+          {
+            type: POST_MESSAGE_RESIZE_TYPE,
+            height: entry.contentRect.height,
+          },
+          "*"
+        );
+      });
+
+      observer.observe(el);
+    } catch (error) {
+      console.error("Browser does not support ResizeObserver");
+    }
+  });
 </script>
 
-<div class="container mf-card" class:embed>
+<div class="container mf-card" class:embed bind:this={el}>
   <Aside>
     <Nav pageHierarchy={utils.getPageHierarchy($cardData?.components)} />
   </Aside>
