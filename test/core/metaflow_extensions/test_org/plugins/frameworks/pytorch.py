e7e91ed8c489ee843eca32d0911248a9212c6edb
Romain <romain-intel@users.noreply.github.com>
2022-11-17 09:35:36 -0800

    Fix `._orig` access for submodules for MF extensions (#1174)
    * Fix `._orig` access for submodules for MF extensions
    
    This addresses an issue where submodules like `mymodule._orig.submodule` would
    not load properly.
    
    This will properly load things like `from .foofa import xyz` from a module or
    sub-module in the original location but currently does not work if the
    import path is absolute.
    
    * Add test that orig module is accessible (#1192)
    
    * Black format
    
    Co-authored-by: Tom Furmston <tfurmston@googlemail.com>

---
+++ test/core/metaflow_extensions/test_org/plugins/frameworks/pytorch.py
@@ -0,0 +1,8 @@
+from metaflow.plugins.frameworks._orig.pytorch import (
+    PytorchParallelDecorator,
+    setup_torch_distributed,
+)
+
+
+class NewPytorchParallelDecorator(PytorchParallelDecorator):
+    pass
