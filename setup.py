4816608dc8b6a56b4a37e5a9c72855947083b601
Savin <savingoyal@gmail.com>
2023-01-19 08:08:26 -0800

    Revert "since we're now providing type annotations, add typed marker PEP-561 (#1239)"
    This reverts commit 7602ad32de1772847cc73b27b8b2f2d17d01d763.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     py_modules=[
         "metaflow",
     ],
-    package_data={"metaflow": ["tutorials/*/*", "py.typed"]},
+    package_data={"metaflow": ["tutorials/*/*"]},
     entry_points="""
         [console_scripts]
         metaflow=metaflow.cmd.main_cli:start
