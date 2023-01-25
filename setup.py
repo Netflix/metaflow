2ff443208305f84fef8fc98167adc604def013d4
Romain Cledat <rcledat@netflix.com>
2023-01-24 22:55:54 -0800

    Bump setup.py to 2.7.20

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = "2.7.19"
+version = "2.7.20"
 
 setup(
     include_package_data=True,
@@ -9,9 +9,28 @@ setup(
     description="Metaflow: More Data Science, Less Engineering",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    author="Machine Learning Infrastructure Team at Netflix",
+    author="Metaflow Developers",
     author_email="help@metaflow.org",
-    license="Apache License 2.0",
+    license="Apache Software License",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    project_urls={
+        "Source": "https://github.com/Netflix/metaflow",
+        "Issues": "https://github.com/Netflix/metaflow/issues",
+        "Documentation": "https://docs.metaflow.org",
+    },
     packages=find_packages(exclude=["metaflow_test"]),
     py_modules=[
         "metaflow",
