5e2171a93c49e469e37206f4a7318d5170bdcd82
Adam Merberg <adam.merberg@equilibriumenergy.com>
2023-01-27 18:06:28 -0500

    fail if timezone is provided with AWS Step Functions

--- metaflow/plugins/aws/step_functions/step_functions.py
+++ metaflow/plugins/aws/step_functions/step_functions.py
@@ -328,6 +328,7 @@ class StepFunctions(object):
 
     def _cron(self):
         schedule = self.flow._flow_decorators.get("schedule")
+        assert schedule.timezone is None, "Step Functions does not support scheduling with a timezone."
         if schedule:
             return schedule.schedule
         return None
