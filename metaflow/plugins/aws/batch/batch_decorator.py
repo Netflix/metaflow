42878dd8b0aa9c6006dc8412dc89e8e8ae4a48bb
Sakari Ikonen <sakari.a.ikonen@gmail.com>
2023-03-29 14:29:29 +0300

    fix handling of tmpfs_tempdir=False

--- metaflow/plugins/aws/batch/batch_decorator.py
+++ metaflow/plugins/aws/batch/batch_decorator.py
@@ -211,7 +211,7 @@ class BatchDecorator(StepDecorator):
         # current.tempdir reflects the value of METAFLOW_TEMPDIR (the current working
         # directory by default), or the value of tmpfs_path if tmpfs_tempdir=False.
         if not self.attributes["tmpfs_tempdir"]:
-            current._update_env("tempdir", self.attributes["tmpfs_path"])
+            current._update_env({"tempdir": self.attributes["tmpfs_path"]})
 
         # task_pre_step may run locally if fallback is activated for @catch
         # decorator. In that scenario, we skip collecting AWS Batch execution
