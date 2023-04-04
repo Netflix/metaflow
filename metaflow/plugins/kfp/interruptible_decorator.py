effef29c027f43c5331fb5036cdad1aa8a9e15e6
Taleb Zeghmi <4167032+talebzeghmi@users.noreply.github.com>
2023-04-04 11:08:50 -0700

    AIP-7275 Save capacity-type, host, instance-type to MF metadata (#234)
    * AIP-7275 Save capacity-type, host, instance-type to MF metadata
    
    Save capacity-type, hostname, instance-type per step within the @interruptible decorator.
    Enables the ability to query for all steps marked @interruptible and whether they ran on spot, and on what nodename and instance-type.

--- metaflow/plugins/kfp/interruptible_decorator.py
+++ metaflow/plugins/kfp/interruptible_decorator.py
@@ -1,4 +1,14 @@
+import os
+from typing import Optional
+
+import requests
 from metaflow.decorators import StepDecorator
+from metaflow.metadata.metadata import MetaDatum
+
+
+def _get_ec2_metadata(path: str) -> Optional[str]:
+    response = requests.get(f"http://169.254.169.254/latest/meta-data/{path}")
+    return response.text
 
 
 class interruptibleDecorator(StepDecorator):
@@ -22,3 +32,32 @@ class interruptibleDecorator(StepDecorator):
     """
 
     name = "interruptible"
+
+    def task_pre_step(
+        self,
+        step_name,
+        task_datastore,
+        metadata,
+        run_id,
+        task_id,
+        flow,
+        graph,
+        retry_count,
+        max_user_code_retries,
+        ubf_context,
+        inputs,
+    ):
+        # Determine whether this is running on Argo, and hence on AWS
+        if os.environ.get("MF_ARGO_WORKFLOW_NAME"):
+            meta = {
+                "capacity-type": _get_ec2_metadata(
+                    "instance-life-cycle"
+                ),  # returns: {on-demand, spot}
+                "hostname": _get_ec2_metadata("hostname"),
+                "instance-type": _get_ec2_metadata("instance-type"),
+            }
+
+            entries = [
+                MetaDatum(field=k, value=v, type=k, tags=[]) for k, v in meta.items()
+            ]
+            metadata.register_metadata(run_id, step_name, task_id, entries)
