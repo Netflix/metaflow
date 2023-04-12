56c9cf541e98d88bc4ca31ad32824bc8a656c0ae
Katie Louise Ross <katielouise.ross@zendesk.com>
2023-04-12 17:27:11 +1000

    update comment with configuration param format

--- metaflow/metaflow_config.py
+++ metaflow/metaflow_config.py
@@ -69,8 +69,8 @@ CLIENT_CACHE_MAX_TASKDATASTORE_COUNT = from_conf(
 S3_ENDPOINT_URL = from_conf("S3_ENDPOINT_URL")
 S3_VERIFY_CERTIFICATE = from_conf("S3_VERIFY_CERTIFICATE")
 
-# Map for custom S3 upload settings
-# Can be used for things like ServerSideEncryption, Tagging, StorageClass, etc. 
+# S3 configurable settings (string of format "KEY=VALUE, KEY=VALUE, etc.")
+# e.g. ServerSideEncryption, Tagging, StorageClass, etc. 
 S3_CUSTOM_UPLOAD_SETTINGS = from_conf("CUSTOM_UPLOAD_SETTINGS_S3")
 
 # S3 retry configuration
