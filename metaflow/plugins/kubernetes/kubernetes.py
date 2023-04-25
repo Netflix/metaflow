91e5ca0dce0583188dd1ecc2e009d9544d0cd417
t-potts <tyler.potts@dtn.com>
2023-04-25 15:48:11 -0500

    correct typo on annotation

--- metaflow/plugins/kubernetes/kubernetes.py
+++ metaflow/plugins/kubernetes/kubernetes.py
@@ -270,7 +270,7 @@ class Kubernetes(object):
 
         (
             job.annotation("metaflow/run_id", run_id)
-            .annotation("autoscaler.kubernetes.io/safe-to-evict", "false")
+            .annotation("cluster-autoscaler.kubernetes.io/safe-to-evict", "false")
             .annotation("metaflow/step_name", step_name)
             .annotation("metaflow/task_id", task_id)
             .annotation("metaflow/attempt", attempt)
