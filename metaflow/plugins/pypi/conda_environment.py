7fb3cdf0d8cf2a80c4334da913bb93805dd979dd
savin <savingoyal@gmail.com>
2023-06-19 12:09:01 -0700

    @pip draft

---
+++ metaflow/plugins/pypi/conda_environment.py
@@ -0,0 +1,313 @@
+import errno
+import fcntl
+import functools
+import json
+import os
+import sys
+from concurrent.futures import ThreadPoolExecutor
+from hashlib import sha256
+
+from metaflow.metaflow_environment import MetaflowEnvironment
+from metaflow.metaflow_profile import profile
+from metaflow.exception import MetaflowException
+
+from urllib.parse import urlparse
+from itertools import chain
+from .utils import conda_platform
+from . import MAGIC_FILE, _datastore_packageroot
+
+
+# TODO: 1. Error handling
+
+
+class CondaEnvironmentException(MetaflowException):
+    headline = "Ran into an error while setting up environment"
+
+    def __init__(self, msg):
+        super(CondaEnvironmentException, self).__init__(msg)
+
+
+class CondaEnvironment(MetaflowEnvironment):
+    TYPE = "conda"
+
+    def __init__(self, flow):
+        self.flow = flow
+
+    def set_local_root(self, local_root):
+        # TODO: Make life simple by passing echo to the constructor and getting rid of
+        # this method's invocation in the decorator
+        self.local_root = local_root
+
+    def decospecs(self):
+        # Apply conda decorator to manage the task execution lifecycle.
+        return ("conda",) + super().decospecs()
+
+    def validate_environment(self, echo, datastore_type):
+        self.datastore_type = datastore_type
+
+        # Initialize necessary virtual environments for all Metaflow tasks.
+        # Use Micromamba for solving conda packages and Pip for solving pypi packages.
+        from .micromamba import Micromamba
+        from .pip import Pip
+
+        self.solvers = {"conda": Micromamba(), "pypi": Pip()}
+
+    def init_environment(self, echo):
+        # The implementation optimizes for latency to ensure as many operations can
+        # be turned into cheap no-ops as feasible. Otherwise, we focus on maintaining
+        # a balance between latency and maintainability of code without re-implementing
+        # the internals of Micromamba and Pip.
+
+        # TODO: Support --datastore=local
+
+        # avoiding circular imports
+        from metaflow.plugins import DATASTORES
+
+        self.storage = [d for d in DATASTORES if d.TYPE == self.datastore_type][0](
+            _datastore_packageroot(self.datastore_type)
+        )
+
+        # TODO: Introduce verbose logging
+        #       https://github.com/Netflix/metaflow/issues/1494
+        echo("Bootstrapping virtual environment(s) ...")
+
+        def environments(type_):
+            seen = set()
+            for step in self.flow:
+                environment = self.get_environment(step)
+                if type_ in environment and environment["id_"] not in seen:
+                    seen.add(environment["id_"])
+                    for platform in environment[type_]["platforms"]:
+                        yield environment["id_"], {
+                            **{
+                                k: v
+                                for k, v in environment[type_].items()
+                                if k != "platforms"
+                            },
+                            **{"platform": platform},
+                        }
+
+        def solve(id_, environment, type_):
+            # Cached solve - should be quick!
+            platform = environment["platform"]
+            return (
+                id_,
+                (
+                    self.read_from_environment_manifest([id_, platform, type_])
+                    or self.write_to_environment_manifest(
+                        [id_, platform, type_],
+                        self.solvers[type_].solve(id_, **environment),
+                    )
+                ),
+                environment["python"],
+                platform,
+            )
+
+        def cache(results, type_):
+            local_packages = {
+                url: {
+                    # Path to package in datastore.
+                    "path": urlparse(url).netloc + urlparse(url).path,
+                    # Path to package on local disk.
+                    "local_path": local_path,
+                }
+                for result in results
+                for url, local_path in self.solvers[type_].metadata(*result).items()
+            }
+            dirty = set()
+            for id_, packages, _, _ in results:
+                for package in packages:
+                    if package.get("path"):
+                        # Cache only those packages that manifest is unaware of
+                        local_packages.pop(package["url"], None)
+                    else:
+                        # TODO: Match up with CONDA_DATASTORE_ROOT so that cache
+                        #       gets invalidated when DATASTORE is moved.
+                        package["path"] = (
+                            urlparse(package["url"]).netloc
+                            + urlparse(package["url"]).path
+                        )
+                        dirty.add(id_)
+            list_of_path_and_filehandle = [
+                (
+                    package["path"],
+                    open(package["local_path"], "rb"),
+                )
+                for package in local_packages.values()
+            ]
+            self.storage.save_bytes(
+                list_of_path_and_filehandle,
+                len_hint=len(list_of_path_and_filehandle),
+            )
+            for id_, packages, _, platform in results:
+                if id_ in dirty:
+                    self.write_to_environment_manifest([id_, platform, type_], packages)
+
+        # First resolve environments through Conda, before PyPI.
+        for solver in ["conda", "pypi"]:
+            with ThreadPoolExecutor() as executor:
+                results = list(
+                    executor.map(lambda x: solve(*x, solver), environments(solver))
+                )
+            _ = list(map(lambda x: self.solvers[solver].download(*x), results))
+            with ThreadPoolExecutor() as executor:
+                _ = list(
+                    executor.map(lambda x: self.solvers[solver].create(*x), results)
+                )
+            cache(results, solver)
+
+    def executable(self, step_name):
+        step = next(step for step in self.flow if step.name == step_name)
+        id_ = self.get_environment(step)["id_"]
+        return os.path.join(id_, "bin/python -s")
+
+    def interpreter(self, step_name):
+        step = next(step for step in self.flow if step.name == step_name)
+        id_ = self.get_environment(step)["id_"]
+        # User workloads are executed through the conda environment's interpreter.
+        return self.solvers["conda"].interpreter(id_)
+
+    @functools.lru_cache(maxsize=None)
+    def get_environment(self, step):
+        environment = {}
+        for decorator in step.decorators:
+            # @conda decorator is guaranteed to exist thanks to self.decospecs
+            if decorator.name in ["conda", "pypi"]:
+                environment[decorator.name] = dict(decorator.attributes)
+
+        # TODO: inject more pinned packages below
+        # Inject pinned packages for supporting `--datastore` and `--metadata`.
+        # PyPI dependencies are prioritized over Conda dependencies.
+        # TODO: Introduce support for `--telemetry` as a follow up.
+        environment.get("pypi", environment["conda"])["packages"] = {
+            **{"requests": "2.31.0", "boto3": "1.14.0"},
+            **environment.get("pypi", environment["conda"])["packages"],
+        }
+
+        # Disallow specifying both @conda and @pypi together for now. Mixing Conda
+        # and PyPI packages comes with a lot of operational pain that we can handle
+        # as follow-up work in the future.
+        if all(
+            map(lambda key: environment.get(key, {}).get("packages"), ["pypi", "conda"])
+        ):
+            msg = "Mixing and matching PyPI packages and Conda packages within a\n"
+            msg += "step is not yet supported. Use one of @pypi or @conda only."
+            raise CondaEnvironmentException(msg)
+
+        # To support cross-platform environments, these invariants are maintained
+        # 1. Conda packages are resolved for target platforms
+        # 2. Conda packages are resolved for local platform only for PyPI packages
+        # 3. Conda environments are created only for local platform
+        # 4. PyPI packages are resolved for target platform within Conda environments
+        #    created for local platform
+        # 5. All resolved packages (Conda or PyPI) are cached
+        # 6. PyPI packages are only installed for local platform
+
+        # Resolve `linux-64` Conda environments if @batch or @kubernetes are in play
+        target_platform = conda_platform()
+        for decorator in step.decorators:
+            if decorator.name in ["batch", "kubernetes"]:
+                # TODO: Support arm architectures
+                target_platform = "linux-64"
+                break
+
+        environment["conda"]["platforms"] = [target_platform]
+        if "pypi" in environment:
+            # For PyPI packages, resolve conda environment for local platform in
+            # addition to target platform
+            environment["conda"]["platforms"] = list(
+                {target_platform, conda_platform()}
+            )
+            environment["pypi"]["platforms"] = [target_platform]
+            environment["pypi"]["python"] = environment["conda"]["python"]
+
+        # Z combinator for a recursive lambda
+        deep_sort = (lambda f: f(f))(
+            lambda f: lambda obj: (
+                {k: f(f)(v) for k, v in sorted(obj.items())}
+                if isinstance(obj, dict)
+                else sorted([f(f)(e) for e in obj])
+                if isinstance(obj, list)
+                else obj
+            )
+        )
+
+        return {
+            **environment,
+            # Create a stable unique id for the environment.
+            "id_": sha256(json.dumps(deep_sort(environment)).encode()).hexdigest()[:15],
+        }
+
+    def pylint_config(self):
+        config = super().pylint_config()
+        # Disable (import-error) in pylint
+        config.append("--disable=F0401")
+        return config
+
+    @classmethod
+    def get_client_info(cls, flow_name, metadata):
+        # TODO: Decide this method's fate
+        return None
+
+    def add_to_package(self):
+        # Add manifest file to job package at the top level.
+        files = []
+        manifest = self.get_environment_manifest_path()
+        if os.path.exists(manifest):
+            files.append((manifest, os.path.basename(manifest)))
+        return files
+
+    def bootstrap_commands(self, step_name, datastore_type):
+        # Bootstrap conda and execution environment for step
+        step = next(step for step in self.flow if step.name == step_name)
+        id_ = self.get_environment(step)["id_"]
+        return [
+            "echo 'Bootstrapping virtual environment...'",
+            'python -m metaflow.plugins.pypi.bootstrap "%s" %s "%s"'
+            % (self.flow.name, id_, self.datastore_type),
+            "echo 'Environment bootstrapped.'",
+            "export PATH=$PATH:/metaflow/bin",
+        ]
+
+    def get_environment_manifest_path(self):
+        return os.path.join(self.local_root, self.flow.name, MAGIC_FILE)
+
+    def read_from_environment_manifest(self, keys):
+        path = self.get_environment_manifest_path()
+        if os.path.exists(path) and os.path.getsize(path) > 0:
+            with open(path) as f:
+                data = json.load(f)
+                for key in keys:
+                    try:
+                        data = data[key]
+                    except:
+                        return None
+                return data
+
+    def write_to_environment_manifest(self, keys, value):
+        path = self.get_environment_manifest_path()
+        try:
+            os.makedirs(os.path.dirname(path))
+        except OSError as x:
+            if x.errno != errno.EEXIST:
+                raise
+        with os.fdopen(os.open(path, os.O_RDWR | os.O_CREAT), "r+") as f:
+            try:
+                fcntl.flock(f, fcntl.LOCK_EX)
+                d = {}
+                if os.path.getsize(path) > 0:
+                    f.seek(0)
+                    d = json.load(f)
+                data = d
+                for key in keys[:-1]:
+                    data = data.setdefault(key, {})
+                data[keys[-1]] = value
+                f.seek(0)
+                json.dump(d, f)
+                f.truncate()
+                return value
+            except IOError as e:
+                if e.errno != errno.EAGAIN:
+                    raise
+            finally:
+                fcntl.flock(f, fcntl.LOCK_UN)
---
+++ metaflow/plugins/pypi/micromamba.py
@@ -0,0 +1,233 @@
+import json
+import os
+import subprocess
+import tempfile
+
+from metaflow.exception import MetaflowException
+from metaflow.util import which
+
+
+class MicromambaException(MetaflowException):
+    headline = "Micromamba ran into an error while setting up environment"
+
+    def __init__(self, error):
+        if isinstance(error, (list,)):
+            error = "\n".join(error)
+        msg = "{error}".format(error=error)
+        super(MicromambaException, self).__init__(msg)
+
+
+# TODO:
+# 1. Verify original .rc is respected
+
+
+class Micromamba(object):
+    def __init__(self):
+        # micromamba is a tiny version of the mamba package manager and comes with
+        # metaflow specific performance enhancements.
+
+        # TODO: Fix micromamba deteection before shipping
+        self.bin = which(
+            os.environ.get("METAFLOW_PATH_TO_MICROMAMBA") or "./micromamba"
+        )
+
+        if self.bin is None:
+            msg = "No installation for *Micromamba* found.\n"
+            msg += "Visit https://mamba.readthedocs.io/en/latest/micromamba-installation.html for installation instructions."
+            raise MetaflowException(msg)
+        # TODO (savin): Introduce a version check for micromamba
+
+    def solve(self, id_, packages, python, platform):
+        # Performance enhancements
+        # 1. Using zstd compressed repodata index files drops the index download time
+        #    by a factor of 10x - conda-forge/noarch/repodata.json has a
+        #    mean download time of 3.705s ± 2.283s vs 385.1ms ± 57.9ms for
+        #    conda-forge/noarch/repodata.json.zst. Thankfully, now micromamba pulls
+        #    zstd compressed files by default - https://github.com/conda-forge/conda-forge.github.io/issues/1835
+        # 2. Tweak repodata ttl to pull either only once a day or if a solve fails -
+        #    this ensures that repodata is not pulled everytime it becomes dirty by
+        #    default. This can result in an environment that has stale transitive
+        #    dependencies but still correct. (--repodata-ttl 86400 --retry-clean-cache)
+        # 3. Introduce pip as python dependency to resolve pip packages within conda
+        #    environment
+        # 4. Multiple solves can progress at the same time while relying on the same
+        #    index
+
+        # TODO: short circuit solve if the solved environment already exists
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            env = {
+                "MAMBA_ADD_PIP_AS_PYTHON_DEPENDENCY": "true",
+                "CONDA_SUBDIR": platform,
+                # "CONDA_UNSATISFIABLE_HINTS_CHECK_DEPTH": "0" # https://github.com/conda/conda/issues/9862
+            }
+            cmd = [
+                "create",
+                "--yes",
+                "--quiet",
+                "--channel=conda-forge",  # TODO: fix this before shipping
+                "--dry-run",
+                "--no-extra-safety-checks",
+                "--repodata-ttl=86400",
+                "--retry-clean-cache",
+                "--prefix=%s/prefix" % tmp_dir,
+            ]
+            for package, version in packages.items():
+                cmd.append("%s==%s" % (package, version))
+            if python:
+                cmd.append("python==%s" % python)
+            return [
+                {k: v for k, v in item.items() if k in ["url"]}
+                for item in self._call(cmd, env)["actions"]["LINK"]
+            ]
+
+    def download(self, id_, packages, python, platform):
+        # Unfortunately all the packages need to be catalogued in package cache
+        # because of which this function can't be parallelized
+
+        # Micromamba is painfully slow in determining if many packages are infact
+        # already cached. As a perf heuristic, we check if the environment already
+        # exists to short circuit package downloads.
+
+        # TODO: Introduce a perf optimization to skip cross-platform downloads
+        #       when already done
+        if self.path_to_environment(id_, platform):
+            return
+
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            env = {
+                "CONDA_SUBDIR": platform,
+            }
+            cmd = [
+                "create",
+                "--yes",
+                "--no-deps",
+                "--download-only",
+                "--safety-checks=disabled",
+                "--no-extra-safety-checks",
+                "--repodata-ttl=86400",
+                "--prefix=%s/prefix" % tmp_dir,
+                "--quiet",
+            ]
+            for package in packages:
+                cmd.append("{url}".format(**package))
+            return self._call(cmd, env)
+
+    def create(self, id_, packages, python, platform):
+        # create environment only if the platform matches system platform
+        if platform != self.platform() or self.path_to_environment(id_, platform):
+            return
+
+        prefix = "{env_dirs}/{keyword}/{platform}/{id}".format(
+            env_dirs=self.info()["envs_dirs"][0],
+            platform=platform,
+            keyword="metaflow",  # indicates metaflow generated environment
+            id=id_,
+        )
+        env = {
+            # "CONDA_PKGS_DIRS": "/Users/savin/micromamba/pkgs/%s" % id_,
+            # use hardlinks when possible, otherwise copy files
+            # disabled for now since it adds to environment creation latencies
+            "CONDA_ALLOW_SOFTLINKS": "0",
+        }
+        cmd = [
+            "create",
+            "--yes",
+            "--no-extra-safety-checks",
+            # "--offline", # not needed since micromamba will first look at the cache
+            "--prefix",  # trick to ensure environments can be created in parallel
+            prefix,
+            "--quiet",
+            "--no-deps",  # important!
+        ]
+        for package in packages:
+            cmd.append("{url}".format(**package))
+        self._call(cmd, env)
+
+    def info(self):
+        return self._call(["config", "list", "-a"])
+
+    def path_to_environment(self, id_, platform=None):
+        if platform is None:
+            platform = self.platform()
+        suffix = "{keyword}/{platform}/{id}".format(
+            platform=platform,
+            keyword="metaflow",  # indicates metaflow generated environment
+            id=id_,
+        )
+        for env in self._call(["env", "list"])["envs"]:
+            # TODO: Check bin/python is available as a heuristic for well formed env
+            if env.endswith(suffix):
+                return env
+
+    def metadata(self, id_, packages, python, platform):
+        # this method unfortunately relies on the implementation detail for
+        # conda environments and has the potential to break all of a sudden.
+        packages_to_filenames = {
+            package["url"]: package["url"].split("/")[-1] for package in packages
+        }
+        directories = self._call(["config", "list", "-a"])["pkgs_dirs"]
+        # search all package caches for packages
+        # TODO: Handle conda clean -a
+        return {
+            url: os.path.join(d, file)
+            for url, file in packages_to_filenames.items()
+            for d in directories
+            if os.path.isdir(d)
+            and file in os.listdir(d)
+            and os.path.isfile(os.path.join(d, file))
+        }
+
+    def interpreter(self, id_):
+        return os.path.join(self.path_to_environment(id_), "bin/python")
+
+    def platform(self):
+        return self.info()["platform"]
+
+    def _call(self, args, env=None):
+        if env is None:
+            env = {}
+        try:
+            result = (
+                subprocess.check_output(
+                    [self.bin] + args,
+                    stderr=subprocess.PIPE,
+                    env={
+                        **os.environ,
+                        # prioritize metaflow-specific env vars
+                        **{k: v for k, v in env.items() if v is not None},
+                        **{
+                            "MAMBA_NO_BANNER": "1",
+                            "MAMBA_JSON": "true",
+                            # play with fire! needed for resolving cross-platform
+                            # environments
+                            "CONDA_SAFETY_CHECKS": "disabled",
+                            # "CONDA_UNSATISFIABLE_HINTS_CHECK_DEPTH": "0",
+                            # Support packages on S3
+                            # "CONDA_ALLOW_NON_CHANNEL_URLS": "1",
+                            "MAMBA_USE_LOCKFILES": "false",
+                        },
+                    },
+                )
+                .decode()
+                .strip()
+            )
+            if result:
+                return json.loads(result)
+            return {}
+        except subprocess.CalledProcessError as e:
+            try:
+                output = json.loads(e.output)
+                err = [output.get("error")]
+                for error in output.get("solver_problems", []):
+                    err.append(error["error"])
+                raise MicromambaException(err)
+            except (TypeError, ValueError) as ve:
+                pass
+            raise MicromambaException(
+                "command '{cmd}' returned error ({code})\n{stderr}".format(
+                    cmd=" ".join(e.cmd),
+                    code=e.returncode,
+                    output=e.output.decode(),
+                    stderr=e.stderr.decode(),
+                )
+            )
