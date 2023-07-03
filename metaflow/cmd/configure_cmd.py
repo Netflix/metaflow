436eee4169f5c93805647b8ac850e7a1e8be0764
Chaoying Wang <chaoyingw@netflix.com>
2023-07-03 02:00:16 +0000

    remove unused import

--- metaflow/cmd/configure_cmd.py
+++ metaflow/cmd/configure_cmd.py
@@ -7,7 +7,6 @@ from os.path import expanduser
 from metaflow.util import to_unicode
 from metaflow._vendor import click
 from metaflow.util import to_unicode
-import metaflow.tracing as tracing
 
 
 from .util import echo_always, makedirs
