64321171b601b886e2038a24893c2e036a9060e6
Riley Hun <riley.hun@autodesk.com>
2023-07-21 17:25:59 -0700

    install ray default

--- metaflow/plugins/frameworks/ray.py
+++ metaflow/plugins/frameworks/ray.py
@@ -14,7 +14,7 @@ class RayParallelDecorator(ParallelDecorator):
     IS_PARALLEL = True
 
     def task_decorate(
-            self, step_func, flow, graph, retry_count, max_user_code_retries, ubf_context
+        self, step_func, flow, graph, retry_count, max_user_code_retries, ubf_context
     ):
         return super().task_decorate(
             step_func, flow, graph, retry_count, max_user_code_retries, ubf_context
@@ -30,6 +30,10 @@ def setup_ray_distributed(master_port=None):
     """
     # Choose port depending on run id to reduce probability of collisions, unless
     # provided by the user.
+    subprocess.run(
+        [sys.executable, "-m", "pip", "install", "ray[default]"]
+    )
+
     try:
         master_port = master_port or (9001 + abs(int(current.run_id)) % 1000)
     except:
@@ -38,7 +42,9 @@ def setup_ray_distributed(master_port=None):
         master_port = 9001
 
     if current.parallel.node_index == 0:
-        subprocess.run([sys.executable, "-m", "ray", "start", "--head", f"--port={master_port}"])
+        subprocess.run(
+            [sys.executable, "-m", "ray", "start", "--head", f"--port={master_port}"]
+        )
     else:
         address = f"{current.parallel.main_ip}:{master_port}"
         subprocess.run([sys.executable, "-m", "ray", "start", "--address", address])
