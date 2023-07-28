6a7000e0aaf75770aec6deb57acc31685f141692
Riley Hun <riley.hun@autodesk.com>
2023-07-27 23:18:16 -1000

    fix ray head node / node connectivity

--- metaflow/plugins/frameworks/ray.py
+++ metaflow/plugins/frameworks/ray.py
@@ -30,21 +30,21 @@ def setup_ray_distributed(master_port=None):
     """
     # Choose port depending on run id to reduce probability of collisions, unless
     # provided by the user.
-    subprocess.run(
-        [sys.executable, "-m", "pip", "install", "ray[default]"]
-    )
+    subprocess.Popen(
+        [sys.executable, "-m", "pip", "install", "-U", "ray[air]"]
+    ).wait()
 
     try:
-        master_port = master_port or (9001 + abs(int(current.run_id)) % 1000)
+        master_port = master_port or (6379 + abs(int(current.run_id)) % 1000)
     except:
         # if `int()` fails, i.e. `run_id` is not an `int`, use just a constant port. Can't use `hash()`,
         # as that is not constant.
-        master_port = 9001
+        master_port = 6379
 
     if current.parallel.node_index == 0:
-        subprocess.run(
-            [sys.executable, "-m", "ray", "start", "--head", f"--port={master_port}"]
-        )
+        subprocess.Popen(f"ray start --head --node-ip-address {current.parallel.main_ip} --port {master_port} --block", shell=True).wait()
     else:
-        address = f"{current.parallel.main_ip}:{master_port}"
-        subprocess.run([sys.executable, "-m", "ray", "start", "--address", address])
+        import ray
+        node_ip_address = ray._private.services.get_node_ip_address()
+        subprocess.Popen(f"ray start --node-ip-address {node_ip_address} --address {current.parallel.main_ip}:{master_port} --block", shell=True).wait()
+
