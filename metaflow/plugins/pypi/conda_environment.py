0f60cc7b3e3814cee66e13b46e75f36caf584a61
Sakari Ikonen <sakari.a.ikonen@gmail.com>
2023-09-13 19:41:30 +0300

    rework so solvers can accept arbitrary environments

--- metaflow/plugins/pypi/conda_environment.py
+++ metaflow/plugins/pypi/conda_environment.py
@@ -77,7 +77,7 @@ class CondaEnvironment(MetaflowEnvironment):
                             **{
                                 k: v
                                 for k, v in environment[type_].items()
-                                if k in ["python", "packages"]
+                                if k != "platform"
                             },
                             **{"platform": platform},
                         }
