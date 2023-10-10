e0bdffc6f1688c4fc7739828b7407fb2dd5e353b
savin <savingoyal@gmail.com>
2023-10-10 10:48:49 -0700

    remove spurious code

--- metaflow/plugins/pypi/micromamba.py
+++ metaflow/plugins/pypi/micromamba.py
@@ -279,7 +279,7 @@ def _install_micromamba(installation_location):
         # https://mamba.readthedocs.io/en/latest/micromamba-installation.html#manual-installation
         # requires bzip2
         result = subprocess.Popen(
-            f"curl -Ls https://micro.mamba.pm/api/micromamba/{platform}/latest | tar -xvj -C {installation_location} bin/micromamba >",
+            f"curl -Ls https://micro.mamba.pm/api/micromamba/{platform}/latest | tar -xvj -C {installation_location} bin/micromamba",
             shell=True,
             stderr=subprocess.PIPE,
             stdout=subprocess.PIPE,
