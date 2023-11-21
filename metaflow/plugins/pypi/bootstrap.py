837bd8668beebc2f858bd1ac0f587f66d39a34ca
Sakari Ikonen <sakari.a.ikonen@gmail.com>
2023-11-21 21:24:16 +0200

    revert unnecessary bootstrap change

--- metaflow/plugins/pypi/bootstrap.py
+++ metaflow/plugins/pypi/bootstrap.py
@@ -124,7 +124,7 @@ if __name__ == "__main__":
             [
                 f"""set -e;
                 export PATH=$PATH:$(pwd)/micromamba;
-                micromamba run --prefix {prefix} pip --disable-pip-version-check install --root-user-action=ignore --no-compile {pypi_pkgs_dir}/*"""
+                micromamba run --prefix {prefix} pip --disable-pip-version-check install --root-user-action=ignore --no-compile {pypi_pkgs_dir}/*.whl"""
             ]
         )
 
