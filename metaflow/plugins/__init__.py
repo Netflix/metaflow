caa10566ac669d6ceab0eabdae5f13199baf9dd4
Oleg Avdeev <oleg.v.avdeev@gmail.com>
2023-12-01 19:10:09 -0800

    pluggable azure credentials provider

--- metaflow/plugins/__init__.py
+++ metaflow/plugins/__init__.py
@@ -122,6 +122,11 @@ SECRETS_PROVIDERS_DESC = [
     ),
 ]
 
+AZURE_CLIENT_PROVIDERS_DESC = [
+    ("azure-default", ".azure.azure_credential.AzureDefaultClientProvider")
+]
+
+
 process_plugins(globals())
 
 
@@ -143,6 +148,7 @@ SIDECARS.update(MONITOR_SIDECARS)
 
 AWS_CLIENT_PROVIDERS = resolve_plugins("aws_client_provider")
 SECRETS_PROVIDERS = resolve_plugins("secrets_provider")
+AZURE_CLIENT_PROVIDERS = resolve_plugins("azure_client_provider")
 
 from .cards.card_modules import MF_EXTERNAL_CARDS
 
