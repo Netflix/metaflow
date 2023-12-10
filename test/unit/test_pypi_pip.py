eac7dcec47969659cdf3b75b2aad63b8f347f17e
Sakari Ikonen <sakari.a.ikonen@gmail.com>
2023-12-11 00:37:09 +0200

    add crossplatform unit tests for platform validation

--- test/unit/test_pypi_pip.py
+++ test/unit/test_pypi_pip.py
@@ -26,6 +26,10 @@ from metaflow.plugins.pypi.pip import verify_wheel_tags_supported
         ("test-1.0-cp39-cp39-macosx_10_12_universal2.whl", "3.9", "osx-64", True),
         ("test-1.0-cp39-cp39-macosx_13_0_universal2.whl", "3.9", "osx-arm64", True),
         ("test-1.0-cp39-cp39-macosx_13_0_universal2.whl", "3.8", "osx-arm64", False),
+        ("test-1.0-cp311-cp311-macosx_13_0_arm64.whl", "3.11", "osx-arm64", True),
+        ("test-1.0-cp311-cp311-macosx_13_0_universal2.whl", "3.11", "linux-64", False),
+        ("test-1.0-cp311-cp311-macosx_13_0_arm64.whl", "3.11", "linux-64", False),
+        ("test-1.0-cp311-cp311-macosx_13_0_x86_64.whl", "3.11", "linux-64", False),
     ],
 )
 def test_verify_wheel_tags_supported(wheel_name, python, platform, expected):
