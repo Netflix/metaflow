9509072229ad2cc86f95c05325f8b82c43474937
leogargu <6627684+leogargu@users.noreply.github.com>
2024-02-05 12:44:34 +0000

    [kubernetes] Use KUBECONFIG if set

--- metaflow/plugins/kubernetes/kubernetes_client.py
+++ metaflow/plugins/kubernetes/kubernetes_client.py
@@ -4,7 +4,9 @@ import time
 
 from metaflow.exception import MetaflowException
 
+from .kubernetes import KubernetesException
 from .kubernetes_job import KubernetesJob
+
 
 CLIENT_REFRESH_INTERVAL_SECONDS = 300
 
@@ -32,15 +34,19 @@ class KubernetesClient(object):
     def _refresh_client(self):
         from kubernetes import client, config
 
-        if os.getenv("KUBERNETES_SERVICE_HOST"):
-            # We are inside a pod, authenticate via ServiceAccount assigned to us
-            config.load_incluster_config()
-        else:
+        if os.getenv("KUBECONFIG"):
             # Use kubeconfig, likely $HOME/.kube/config
             # TODO (savin):
             #  1. Support generating kubeconfig on the fly using boto3
             #  2. Support auth via OIDC - https://docs.aws.amazon.com/eks/latest/userguide/authenticate-oidc-identity-provider.html
             config.load_kube_config()
+        elif os.getenv("KUBERNETES_SERVICE_HOST"):
+            # We are inside a pod, authenticate via ServiceAccount assigned to us
+            config.load_incluster_config()
+        else:
+            raise KubernetesException(
+                "Neither KUBECONFIG exists nor are we running inside a cluster."
+            )
         self._client = client
         self._client_refresh_timestamp = time.time()
 
