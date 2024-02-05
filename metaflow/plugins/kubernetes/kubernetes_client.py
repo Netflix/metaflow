f9ffb10024d7b2dd7502bc103ca6824769667a0d
leogargu <6627684+leogargu@users.noreply.github.com>
2024-02-05 18:57:22 +0000

    [kubernetes] If neither KUBECONFIG nor KUBERNETES_SERVICE_HOST are set, default to loading kubeconfig to keep current behaviour

--- metaflow/plugins/kubernetes/kubernetes_client.py
+++ metaflow/plugins/kubernetes/kubernetes_client.py
@@ -4,7 +4,6 @@ import time
 
 from metaflow.exception import MetaflowException
 
-from .kubernetes import KubernetesException
 from .kubernetes_job import KubernetesJob
 
 
@@ -35,18 +34,16 @@ class KubernetesClient(object):
         from kubernetes import client, config
 
         if os.getenv("KUBECONFIG"):
-            # Use kubeconfig, likely $HOME/.kube/config
-            # TODO (savin):
-            #  1. Support generating kubeconfig on the fly using boto3
-            #  2. Support auth via OIDC - https://docs.aws.amazon.com/eks/latest/userguide/authenticate-oidc-identity-provider.html
             config.load_kube_config()
         elif os.getenv("KUBERNETES_SERVICE_HOST"):
             # We are inside a pod, authenticate via ServiceAccount assigned to us
             config.load_incluster_config()
         else:
-            raise KubernetesException(
-                "Neither KUBECONFIG exists nor are we running inside a cluster."
-            )
+            # Default to using kubeconfig, likely $HOME/.kube/config
+            # TODO (savin):
+            #  1. Support generating kubeconfig on the fly using boto3
+            #  2. Support auth via OIDC - https://docs.aws.amazon.com/eks/latest/userguide/authenticate-oidc-identity-provider.html
+            config.load_kube_config()
         self._client = client
         self._client_refresh_timestamp = time.time()
 
