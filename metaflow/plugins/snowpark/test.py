621e3070026b73095b87478effb1c40012fa89da
savin <savingoyal@gmail.com>
2024-02-22 12:13:38 -0800

    mo work

--- metaflow/plugins/snowpark/test.py
+++ metaflow/plugins/snowpark/test.py
@@ -1,13 +1,18 @@
 import snowflake.connector
 from snowflake.connector.errors import ProgrammingError
+from snowflake.connector import DictCursor
 
 class SnowflakeAsyncClient(object):
 
-    def __init__(self, user, password, account):
+    def __init__(self, user, password, account, role="test_role",warehouse="TUTORIAL_WAREHOUSE", database="TUTORIAL_DB",  schema="TUTORIAL_DB.DATA_SCHEMA"):
         self.ctx = snowflake.connector.connect(
             user=user,
             password=password,
+            role=role,
             account=account,
+            warehouse=warehouse,
+            database=database,
+            schema=schema
         )
         self.ctx.autocommit(True)
 
@@ -41,4 +46,9 @@ class SnowflakeAsyncClient(object):
             return None
 
 if __name__ == "__main__":
-	SnowflakeAsyncClient(user="SAVINOUTERBOUNDS", password="Thefray7.", account="BDHLWAK.MKB21361")
+    client = SnowflakeAsyncClient(user="SAVINOUTERBOUNDS", password="Thefray7", account="pfb48862")
+    _id = client.execute_query_async("PUT file:///Users/savin/Downloads/SnowparkContainerServices-Tutorials/Tutorial-2/my_job_spec.yaml @TUTORIAL_DB.DATA_SCHEMA.TUTORIAL_STAGE AUTO_COMPRESS=FALSE OVERWRITE=TRUE;")
+    print(_id)
+    _id = client.execute_query_async("EXECUTE SERVICE IN COMPUTE POOL tutorial_compute_pool FROM @tutorial_stage SPEC='my_job_spec.yaml';")
+    print(_id)
+    print(client.check_query_status(_id))
