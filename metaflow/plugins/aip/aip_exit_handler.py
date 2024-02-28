d90208005dd46ef74cc1ea9b0f0356618cc2bac9
Taleb Zeghmi <talebz@zillowgroup.com>
2024-02-28 14:52:45 -0800

    AIP-8098 ExitHandler doesn't notify on Workflow Error

--- metaflow/plugins/aip/aip_exit_handler.py
+++ metaflow/plugins/aip/aip_exit_handler.py
@@ -143,10 +143,12 @@ def exit_handler(
         notify_on_error = get_env("METAFLOW_NOTIFY_ON_ERROR")
         notify_on_success = get_env("METAFLOW_NOTIFY_ON_SUCCESS")
 
-        if notify_on_error and status == "Failed":
-            email_notify(notify_on_error)
-        elif notify_on_success and status == "Succeeded":
+        # AIP-8098 ExitHandler and Ops notification NOT called on Workflow.status == Error
+        #   available statuses of Succeeded, Failed, Error
+        if notify_on_success and status == "Succeeded":
             email_notify(notify_on_success)
+        elif notify_on_error:
+            email_notify(notify_on_error)
         else:
             print("No notification is necessary!")
 
