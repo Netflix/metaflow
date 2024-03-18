6257d47cae4ec57c63759849e7a19acc2a63e2ec
Sakari Ikonen <sakari.a.ikonen@gmail.com>
2024-03-18 14:02:43 +0200

    apply black

--- metaflow/plugins/argo/argo_workflows.py
+++ metaflow/plugins/argo/argo_workflows.py
@@ -322,6 +322,7 @@ class ArgoWorkflows(object):
         Append Argo specific annotations that are unavailable at the decorator level.
         """
         from datetime import datetime, timezone
+
         resources = dict(
             [
                 deco
