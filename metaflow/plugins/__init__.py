166333a81e7d5066b2d1a53ddb7ff20e22025d14
Sakari Ikonen <sakari.a.ikonen@gmail.com>
2024-04-15 12:28:13 +0300

    move imagebakery files under docker-plugin

--- metaflow/plugins/__init__.py
+++ metaflow/plugins/__init__.py
@@ -66,7 +66,7 @@ FLOW_DECORATORS_DESC = [
 ENVIRONMENTS_DESC = [
     ("conda", ".pypi.conda_environment.CondaEnvironment"),
     ("pypi", ".pypi.pypi_environment.PyPIEnvironment"),
-    ("docker", ".pypi.docker_environment.DockerEnvironment"),
+    ("docker", ".docker.docker_environment.DockerEnvironment"),
 ]
 
 # Add metadata providers here
