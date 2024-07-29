9638d06dd8d572f5504b3aad0addcd081dd79591
Adam Merberg <adam.merberg@equilibriumenergy.com>
2024-07-29 10:19:29 -0400

    Show link to argo workflows when triggering a run

--- metaflow/plugins/argo/argo_workflows_cli.py
+++ metaflow/plugins/argo/argo_workflows_cli.py
@@ -963,7 +963,8 @@ def trigger(obj, run_id_file=None, deployer_attribute_file=None, **kwargs):
             obj.echo("re-deploy your flow in order to get rid of this message.")
             workflow_name_to_deploy = obj._v1_workflow_name
     response = ArgoWorkflows.trigger(workflow_name_to_deploy, params)
-    run_id = "argo-" + response["metadata"]["name"]
+    argo_workflow_id = response["metadata"]["name"]
+    run_id = "argo-" + argo_workflow_id
 
     if run_id_file:
         with open(run_id_file, "w") as f:
@@ -985,6 +986,23 @@ def trigger(obj, run_id_file=None, deployer_attribute_file=None, **kwargs):
         "(run-id *{run_id}*).".format(name=workflow_name_to_deploy, run_id=run_id),
         bold=True,
     )
+
+    workflow_url = (
+        "%s/workflows/%s/%s"
+        % (
+            ARGO_WORKFLOWS_UI_URL.rstrip("/"),
+            KUBERNETES_NAMESPACE,
+            argo_workflow_id,
+        )
+        if ARGO_WORKFLOWS_UI_URL
+        else None
+    )
+
+    if workflow_url:
+        obj.echo(
+            "See it in the Argo Workflows UI at %s" % workflow_url,
+            bold=True,
+        )
 
     run_url = (
         "%s/%s/%s" % (UI_URL.rstrip("/"), obj.flow.name, run_id) if UI_URL else None
