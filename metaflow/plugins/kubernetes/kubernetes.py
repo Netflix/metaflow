97c0dd1621b0145a456e8afbf3382d1fb448caa6
Madhur Tandon <madhur@outerbounds.co>
2024-08-08 23:14:06 +0530

    use the arg instead

--- metaflow/plugins/kubernetes/kubernetes.py
+++ metaflow/plugins/kubernetes/kubernetes.py
@@ -34,7 +34,6 @@ from metaflow.metaflow_config import (
     GCP_SECRET_MANAGER_PREFIX,
     KUBERNETES_FETCH_EC2_METADATA,
     KUBERNETES_LABELS,
-    KUBERNETES_NAMESPACE,
     KUBERNETES_SANDBOX_INIT_SCRIPT,
     OTEL_ENDPOINT,
     S3_ENDPOINT_URL,
@@ -282,7 +281,7 @@ class Kubernetes(object):
                 "METAFLOW_INIT_SCRIPT", KUBERNETES_SANDBOX_INIT_SCRIPT
             )
             .environment_variable("METAFLOW_OTEL_ENDPOINT", OTEL_ENDPOINT)
-            .environment_variable("METAFLOW_KUBERNETES_NAMESPACE", KUBERNETES_NAMESPACE)
+            .environment_variable("METAFLOW_KUBERNETES_NAMESPACE", namespace)
             # Skip setting METAFLOW_DATASTORE_SYSROOT_LOCAL because metadata sync
             # between the local user instance and the remote Kubernetes pod
             # assumes metadata is stored in DATASTORE_LOCAL_DIR on the Kubernetes
@@ -567,7 +566,7 @@ class Kubernetes(object):
                 "METAFLOW_INIT_SCRIPT", KUBERNETES_SANDBOX_INIT_SCRIPT
             )
             .environment_variable("METAFLOW_OTEL_ENDPOINT", OTEL_ENDPOINT)
-            .environment_variable("METAFLOW_KUBERNETES_NAMESPACE", KUBERNETES_NAMESPACE)
+            .environment_variable("METAFLOW_KUBERNETES_NAMESPACE", namespace)
             # Skip setting METAFLOW_DATASTORE_SYSROOT_LOCAL because metadata sync
             # between the local user instance and the remote Kubernetes pod
             # assumes metadata is stored in DATASTORE_LOCAL_DIR on the Kubernetes
