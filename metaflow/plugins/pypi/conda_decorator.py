bb839e0dd03df75d4175dd698eac440f537f47f1
Sakari Ikonen <sakari.a.ikonen@gmail.com>
2024-08-19 12:33:35 +0300

    rename docker environment to fast-bakery

--- metaflow/plugins/pypi/conda_decorator.py
+++ metaflow/plugins/pypi/conda_decorator.py
@@ -100,9 +100,9 @@ class CondaStepDecorator(StepDecorator):
         # --environment=pypi to --environment=conda
         _supported_virtual_envs.extend(["pypi"])
 
-        # TODO: Hardcoded for now to support Docker environment.
+        # TODO: Hardcoded for now to support the fast bakery environment.
         # We should introduce a more robust mechanism for appending supported environments, for example from within extensions.
-        _supported_virtual_envs.extend(["docker"])
+        _supported_virtual_envs.extend(["fast-bakery"])
 
         # The --environment= requirement ensures that valid virtual environments are
         # created for every step to execute it, greatly simplifying the @conda
@@ -344,9 +344,9 @@ class CondaFlowDecorator(FlowDecorator):
         # --environment=pypi to --environment=conda
         _supported_virtual_envs.extend(["pypi"])
 
-        # TODO: Hardcoded for now to support Docker environment.
+        # TODO: Hardcoded for now to support the fast bakery environment.
         # We should introduce a more robust mechanism for appending supported environments, for example from within extensions.
-        _supported_virtual_envs.extend(["docker"])
+        _supported_virtual_envs.extend(["fast-bakery"])
 
         # The --environment= requirement ensures that valid virtual environments are
         # created for every step to execute it, greatly simplifying the @conda
