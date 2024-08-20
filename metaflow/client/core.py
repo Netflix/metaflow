2bf2a610ec09dca3a7df37c2de0fc9bff5713198
Mut Ze <mutongsoft@gmail.com>
2024-08-21 01:30:29 +0800

    Raise AttributeError on non-exist artifact (#1971)

--- metaflow/client/core.py
+++ metaflow/client/core.py
@@ -1027,6 +1027,8 @@ class MetaflowData(object):
         self._artifacts = dict((art.id, art) for art in artifacts)
 
     def __getattr__(self, name: str):
+        if name not in self._artifacts:
+            raise AttributeError(name)
         return self._artifacts[name].data
 
     def __contains__(self, var):
