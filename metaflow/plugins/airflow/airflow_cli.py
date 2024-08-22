1ed3e743f1b0a6de6ba43adef3caa30837dc7afa
Madhur Tandon <madhur@outerbounds.co>
2024-08-23 00:56:43 +0530

    only for argo

--- metaflow/plugins/airflow/airflow_cli.py
+++ metaflow/plugins/airflow/airflow_cli.py
@@ -389,21 +389,6 @@ def _validate_workflow(flow, graph, flow_datastore, metadata, workflow_timeout):
                 "Step *%s* is marked for execution on AWS Batch with Airflow which isn't currently supported."
                 % node.name
             )
-        if any([d.name == "snowpark" for d in node.decorators]):
-            raise MetaflowException(
-                "Step *%s* is marked for execution on Snowpark with Airflow which isn't currently supported."
-                % node.name
-            )
-        if any([d.name == "slurm" for d in node.decorators]):
-            raise MetaflowException(
-                "Step *%s* is marked for execution on Slurm with Airflow which isn't currently supported."
-                % node.name
-            )
-        if any([d.name == "nvidia" for d in node.decorators]):
-            raise MetaflowException(
-                "Step *%s* is marked for execution on Nvidia with Airflow which isn't currently supported."
-                % node.name
-            )
     SUPPORTED_DATASTORES = ("azure", "s3", "gs")
     if flow_datastore.TYPE not in SUPPORTED_DATASTORES:
         raise AirflowException(
