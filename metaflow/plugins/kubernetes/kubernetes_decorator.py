449cadb61026fe7be3c82a5fd5dbe3c197811712
Sakari Ikonen <sakari.a.ikonen@gmail.com>
2024-09-09 11:49:28 +0300

    correct docstring for node_selector string type

--- metaflow/plugins/kubernetes/kubernetes_decorator.py
+++ metaflow/plugins/kubernetes/kubernetes_decorator.py
@@ -72,7 +72,7 @@ class KubernetesDecorator(StepDecorator):
         in Metaflow configuration.
     node_selector: Union[Dict[str,str], str], optional, default None
         Kubernetes node selector(s) to apply to the pod running the task.
-        Can be passed in as a space separated string of values e.g. "kubernetes.io/os=linux kubernetes.io/arch=amd64"
+        Can be passed in as a comma separated string of values e.g. "kubernetes.io/os=linux,kubernetes.io/arch=amd64"
         or as a dictionary {"kubernetes.io/os": "linux", "kubernetes.io/arch": "amd64"}
     namespace : str, default METAFLOW_KUBERNETES_NAMESPACE
         Kubernetes namespace to use when launching pod in Kubernetes.
