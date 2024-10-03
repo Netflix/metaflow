7ed220e89491390e33ed31344d20ab08e747a2ec
Filip Cacky <cacky.filip@gmail.com>
2024-10-03 15:16:06 +0200

    Fix async_read_from_file_when_ready

--- metaflow/runner/utils.py
+++ metaflow/runner/utils.py
@@ -72,14 +72,23 @@ def read_from_file_when_ready(
 async def async_read_from_file_when_ready(
     file_path: str, command_obj: "CommandManager", timeout: float = 5
 ):
-    await asyncio.wait_for(command_obj.process.wait(), timeout)
-
+    start_time = time.time()
     with open(file_path, "r", encoding="utf-8") as file_pointer:
         content = file_pointer.read()
-        if not content:
-            raise CalledProcessError(
-                command_obj.process.returncode, command_obj.command
-            )
+        while not content:
+            if check_process_status(command_obj):
+                content = file_pointer.read()
+                if content:
+                    break
+                raise CalledProcessError(
+                    command_obj.process.returncode, command_obj.command
+                )
+            if time.time() - start_time > timeout:
+                raise TimeoutError(
+                    "Timeout while waiting for file content from '%s'" % file_path
+                )
+            await asyncio.sleep(0.1)
+            content = file_pointer.read()
         return content
 
 
