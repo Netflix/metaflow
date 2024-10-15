93f7f50495f38b25609ef4ed6d6f57003e6a3ca5
Sakari Ikonen <sakari.a.ikonen@gmail.com>
2024-10-15 18:22:25 +0300

    remove py35 from package classifiers

--- setup.py
+++ setup.py
@@ -19,7 +19,6 @@ setup(
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
