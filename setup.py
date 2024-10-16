de168b0720e229596cc0f559324bf7401717169e
Savin <savingoyal@gmail.com>
2024-10-16 08:51:00 -0700

    drop py3.5 / add py3.13 (#2081)
    * remove 3.5 from tests
    
    * remove py35 from package classifiers
    
    * add 3.13
    
    * add 3.13
    
    ---------
    
    Co-authored-by: Sakari Ikonen <sakari.a.ikonen@gmail.com>

--- setup.py
+++ setup.py
@@ -19,7 +19,6 @@ setup(
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
@@ -27,6 +26,7 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
     ],
     project_urls={
         "Source": "https://github.com/Netflix/metaflow",
