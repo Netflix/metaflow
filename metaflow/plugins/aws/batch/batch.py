c100fc7398dcf8f80e75d802d6cc63154e314ddf
RikishK <rikishkamboj@gmail.com>
2024-11-14 10:24:11 +1100

    step functions aws_tags

--- metaflow/plugins/aws/batch/batch.py
+++ metaflow/plugins/aws/batch/batch.py
@@ -309,6 +309,7 @@ class Batch(object):
         # Tags for AWS Batch job (for say cost attribution)
         if BATCH_EMIT_TAGS:
             job.tag("app", "metaflow")
+            job.tag("metaflow-tag-fork", "hello")
             for key in [
                 "metaflow.flow_name",
                 "metaflow.run_id",
