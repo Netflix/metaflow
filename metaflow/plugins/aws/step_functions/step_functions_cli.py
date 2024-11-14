9f8d851938ee986d9f5b70d84c88c52004693582
RikishK <rikishkamboj@gmail.com>
2024-11-14 11:18:15 +1100

    type and debug help

--- metaflow/plugins/aws/step_functions/step_functions_cli.py
+++ metaflow/plugins/aws/step_functions/step_functions_cli.py
@@ -480,7 +480,7 @@ def resolve_token(
     default=None,
     show_default=True,
     type=str,
-    help="Write the ID of this run to the file specified.",
+    help="Write the ID of this run to the file specified. TESTING",
 )
 @click.option(
     "--deployer-attribute-file",
@@ -493,6 +493,7 @@ def resolve_token(
 @click.option(
     "--aws-tags",
     multiple=True,
+    type=dict,
     default=None,
     help="AWS tags.")
 @click.pass_obj
