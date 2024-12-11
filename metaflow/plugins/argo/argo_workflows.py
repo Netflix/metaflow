81f5ee3b182966302b98b81e8556a4044839d9ac
Sakari Ikonen <sakari.a.ikonen@gmail.com>
2024-12-11 16:16:52 +0200

    make dag info available through argo workflow template annotation

--- metaflow/plugins/argo/argo_workflows.py
+++ metaflow/plugins/argo/argo_workflows.py
@@ -743,6 +743,18 @@ class ArgoWorkflows(object):
                     )
                 }
             )
+        try:
+            # Build the DAG based on the DAGNodes given by the FlowGraph for the found FlowSpec class.
+            steps_info, graph_structure = self.graph.output_steps()
+            graph_info = {
+                "steps": steps_info,
+                "graph_structure": graph_structure,
+                "doc": self.graph.doc,
+            }
+        except Exception:
+            graph_info = None
+
+        dag_annotation = {"metaflow/dag": json.dumps(graph_info)}
 
         return (
             WorkflowTemplate()
@@ -758,6 +770,7 @@ class ArgoWorkflows(object):
                 .annotations(self._base_annotations)
                 .labels(self._base_labels)
                 .label("app.kubernetes.io/name", "metaflow-flow")
+                .annotations(dag_annotation)
             )
             .spec(
                 WorkflowSpec()
