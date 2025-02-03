b36e1c48093de5cc86ecc519629d9c86069fe916
RikishK <rikishkamboj@gmail.com>
2025-02-03 16:31:35 +1100

    "aws_tags" consistency

--- metaflow/plugins/aws/batch/batch_decorator.py
+++ metaflow/plugins/aws/batch/batch_decorator.py
@@ -70,7 +70,7 @@ class BatchDecorator(StepDecorator):
         A swappiness value of 0 causes swapping not to happen unless absolutely
         necessary. A swappiness value of 100 causes pages to be swapped very
         aggressively. Accepted values are whole numbers between 0 and 100.
-    tags: map, optional
+    tags: Dict[str, str], optional
         Sets arbitrary AWS tags on the AWS Batch compute environment.
         Set as string key-value pairs.
     use_tmpfs : bool, default False
