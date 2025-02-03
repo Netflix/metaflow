9ddbd3a06bcc8cdf604aae10fbb0ce6501d457ca
Mike Macpherson <mmacpherson@users.noreply.github.com>
2025-02-03 12:52:14 -0800

    keep typeguard compatible with 3.7; downgrade `vendor_any` typeguard to keep 3.9 working

--- metaflow/_vendor/v3_7/typeguard/_union_transformer.py
+++ metaflow/_vendor/v3_7/typeguard/_union_transformer.py
@@ -2,7 +2,6 @@
 Transforms lazily evaluated PEP 604 unions into typing.Unions, for compatibility with
 Python versions older than 3.10.
 """
-
 from __future__ import annotations
 
 from ast import (
