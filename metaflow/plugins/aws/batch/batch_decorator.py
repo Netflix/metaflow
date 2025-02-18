268ad74be8688b23a466f5592867032325fb8795
Sakari Ikonen <sakari.a.ikonen@gmail.com>
2025-02-18 12:58:16 +0200

    correct keying. also apply black

--- metaflow/plugins/aws/batch/batch_decorator.py
+++ metaflow/plugins/aws/batch/batch_decorator.py
@@ -186,31 +186,30 @@ class BatchDecorator(StepDecorator):
 
         if self.attributes["trainium"] is not None:
             self.attributes["inferentia"] = self.attributes["trainium"]
-           
-        
-        print(f"Recieved BATCH_DEFAULT_TAGS as: {metaflow_config.BATCH_DEFAULT_TAGS}")
-        if not isinstance(metaflow_config.BATCH_DEFAULT_TAGS, dict):
-            raise BatchException("BATCH_DEFAULT_TAGS environment variable must be Dict[str, str]")
-        if not all(isinstance(k, str) and isinstance(v, str) for k, v in metaflow_config.BATCH_DEFAULT_TAGS.items()):
-            raise BatchException("BATCH_DEFAULT_TAGS environment variable must be Dict[str, str]")
 
-        if self.attributes["aws_batch_tags"] is None and metaflow_config.BATCH_DEFAULT_TAGS != {}:
-            self.attributes["aws_batch_tags"] = metaflow_config.BATCH_DEFAULT_TAGS
+        if not isinstance(BATCH_DEFAULT_TAGS, dict) and not all(
+            instance(k, str) and isinstance(v, str)
+            for k, v in BATCH_DEFAULT_TAGS.items()
+        ):
+            raise BatchException(
+                "BATCH_DEFAULT_TAGS environment variable must be Dict[str, str]"
+            )
+        if self.attributes["aws_batch_tags"] is None:
+            self.attributes["aws_batch_tags"] = BATCH_DEFAULT_TAGS
 
         if self.attributes["aws_batch_tags"] is not None:
-            if not isinstance(self.attributes["aws_batch_tags"], dict):
-                raise BatchException("aws_batch_tags is not a dictionary must be Dict[str, str]")
-            print(f'Recieved Decorator AWS Tags Dictionary as: {self.attributes["aws_batch_tags"]}' )
-            if not all(isinstance(k, str) and isinstance(v, str) for k, v in self.attributes["aws_batch_tags"].items()):
-                raise BatchException("Not all keys and values in aws_batch_tags are strings, must be Dict[str, str]")
-            
-            if metaflow_config.BATCH_DEFAULT_TAGS is not {}: 
-                self.attributes["aws_batch_tags"] = {
-                    **metaflow_config.BATCH_DEFAULT_TAGS,
-                    **self.attributes["aws_batch_tags"],
-                    }
+            if not isinstance(self.attributes["aws_batch_tags"], dict) and not all(
+                isinstance(k, str) and isinstance(v, str)
+                for k, v in self.attributes["aws_batch_tags"].items()
+            ):
+                raise BatchException("aws_batch_tags must be Dict[str, str]")
 
-            decorator_aws_batch_tags_list = [
+            batch_default_tags_copy = BATCH_DEFAULT_TAGS.copy()
+            self.attributes["aws_batch_tags"] = batch_default_tags_copy.update(
+                self.attributes["aws_batch_tags"]
+            )
+
+            decorator_aws_tags_list = [
                 {"key": key, "value": val}
                 for key, val in self.attributes["aws_batch_tags"].items()
             ]
