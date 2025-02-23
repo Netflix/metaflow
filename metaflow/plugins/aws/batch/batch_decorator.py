1fc74686dab014ac5b47b174c79b45011e44a4c5
RikishK <rikishkamboj@gmail.com>
2025-02-24 10:41:06 +1100

    Fixup BATCH_DEFAULT_TAGS merge to decorator batch tags

--- metaflow/plugins/aws/batch/batch_decorator.py
+++ metaflow/plugins/aws/batch/batch_decorator.py
@@ -183,17 +183,19 @@ class BatchDecorator(StepDecorator):
         if self.attributes["trainium"] is not None:
             self.attributes["inferentia"] = self.attributes["trainium"]
 
-        if not isinstance(BATCH_DEFAULT_TAGS, dict) and not all(instance(k, str) and isinstance(v, str) for k, v in BATCH_DEFAULT_TAGS.items()):
-            raise BatchException("BATCH_DEFAULT_TAGS environment variable must be Dict[str, str]")
-        if self.attributes["aws_batch_tags"] is None:
-            self.attributes["aws_batch_tags"] = BATCH_DEFAULT_TAGS
+        if BATCH_DEFAULT_TAGS is not None:
+            if not isinstance(BATCH_DEFAULT_TAGS, dict) and not all(isinstance(k, str) and isinstance(v, str) for k, v in BATCH_DEFAULT_TAGS.items()):
+                raise BatchException("BATCH_DEFAULT_TAGS environment variable must be Dict[str, str]")
+            if self.attributes["aws_batch_tags"] is None:
+                self.attributes["aws_batch_tags"] = BATCH_DEFAULT_TAGS
 
         if self.attributes["aws_batch_tags"] is not None:
             if not isinstance(self.attributes["aws_batch_tags"], dict) and not all(isinstance(k, str) and isinstance(v, str) for k, v in self.attributes["aws_batch_tags"].items()):
                 raise BatchException("aws_batch_tags must be Dict[str, str]")
             
-            batch_default_tags_copy = BATCH_DEFAULT_TAGS.copy()
-            self.attributes["aws_batch_tags"] = batch_default_tags_copy.update(self.attributes["aws_batch_tags"]) 
+            if BATCH_DEFAULT_TAGS is not None:
+                batch_default_tags_copy = BATCH_DEFAULT_TAGS.copy()
+                self.attributes["aws_batch_tags"] = batch_default_tags_copy.update(self.attributes["aws_batch_tags"]) 
 
             decorator_aws_batch_tags_list = [
                 {'key': key,
