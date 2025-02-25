006b662e9e78588f5ab040b90845642bc23d36e5
savin <savingoyal@gmail.com>
2025-02-24 19:08:20 -0800

    mo changes

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
+import os, glob
 from setuptools import setup, find_packages
 
 with open("metaflow/version.py", mode="r") as f:
     version = f.read().splitlines()[0].split("=")[1].strip(" \"'")
 
+
+def find_devtools_files():
+    print("Finding devtools files")
+    filepaths = []
+    for path in glob.iglob("devtools/**/*", recursive=True):
+        if os.path.isfile(path):
+            filepaths.append(path)
+    print(filepaths)
+    return filepaths
+
+
 setup(
     include_package_data=True,
     name="metaflow",
     version=version,
-    description="Metaflow: More Data Science, Less Engineering",
+    description="Metaflow: More AI/ML, Less Engineering",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="Metaflow Developers",
@@ -45,9 +57,11 @@ setup(
             "**/*.pyi",
         ]
     },
+    data_files=[("share/metaflow/devtools", find_devtools_files())],
     entry_points="""
         [console_scripts]
         metaflow=metaflow.cmd.main_cli:start
+        metaflow-dev=metaflow.cmd.make_wrapper:main
       """,
     install_requires=["requests", "boto3"],
     extras_require={
