af25096e1e9faf8ecd42a2279af939064e0362dc
Sakari Ikonen <sakari.a.ikonen@gmail.com>
2025-03-03 15:42:04 +0200

    adding comment to pip patcher script explaining use case

--- metaflow/plugins/pypi/pip_patcher.py
+++ metaflow/plugins/pypi/pip_patcher.py
@@ -3,6 +3,9 @@ import os
 import sys
 
 
+# Because Pip does not offer a direct way to set target platform_system and platform_machine values for resolving packages transitive dependencies, we need to instead
+# manually patch the correct target architecture values for pip to be able to resolve the whole dependency tree successfully.
+# This is necessary for packages that have conditional dependencies dependent on machine/system, e.g. Torch
 @patch("platform.machine", lambda: os.environ.get("PIP_PATCH_MACHINE", "x86_64"))
 @patch("platform.system", lambda: os.environ.get("PIP_PATCH_SYSTEM", "Linux"))
 def _main(args):
