9f7faa5c6dccccfe2d75e60d3ceae666ff886617
Sakari Ikonen <sakari.a.ikonen@gmail.com>
2025-03-11 00:23:07 +0200

    cleanup service filter_tasks_by_metadata. handle _request usage correctly

--- metaflow/plugins/metadata_providers/service.py
+++ metaflow/plugins/metadata_providers/service.py
@@ -353,12 +353,11 @@ class ServiceMetadataProvider(MetadataProvider):
         query_params = {
             "metadata_field_name": field_name,
             "pattern": pattern,
-            "step_name": step_name,
         }
         url = ServiceMetadataProvider._obj_path(flow_name, run_id, step_name)
         url = f"{url}/filtered_tasks?{urlencode(query_params)}"
         try:
-            resp = cls._request(None, url, "GET")
+            resp, _ = cls._request(None, url, "GET")
         except Exception as e:
             if e.http_code == 404:
                 # filter_tasks_by_metadata endpoint does not exist in the version of metadata service
@@ -367,6 +366,8 @@ class ServiceMetadataProvider(MetadataProvider):
                     "The version of metadata service deployed currently does not support filtering tasks by metadata. "
                     "Upgrade Metadata service to version 2.15 or greater to use this feature."
                 ) from e
+            # Other unknown exception
+            raise e
         return resp
 
     @staticmethod
