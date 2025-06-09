40267bfc62f8e1cc6f94402f62753d0638aa5217
Sakari Ikonen <sakari.a.ikonen@gmail.com>
2025-06-09 16:04:25 +0300

    add prefixes to scripthook name

--- metaflow/plugins/argo/hook_decorator.py
+++ metaflow/plugins/argo/hook_decorator.py
@@ -1,6 +1,7 @@
 from metaflow.decorators import FlowDecorator
 import inspect
 
+from metaflow.exception import MetaflowException
 from metaflow.plugins.argo.exit_hooks import ScriptHook
 
 
@@ -19,11 +20,23 @@ class RunOnFinishDecorator(FlowDecorator):
     def flow_init(
         self, flow, graph, environment, flow_datastore, metadata, logger, echo, options
     ):
+        on_success = self.attributes["on_success"]
+        on_error = self.attributes["on_error"]
+
+        if not on_success and not on_error:
+            raise MetaflowException("Choose one of the options on_success/on_error")
+
+        prefix = ""
+        if on_success:
+            prefix = "success"
+        elif on_error:
+            prefix = "error"
+
         self.hooks = []
         for fn in self.attributes["functions"]:
             self.hooks.append(
                 ScriptHook(
-                    name=f"success-{fn.__name__}",
+                    name=f"{prefix}-{fn.__name__}",
                     language=self.attributes["language"],
                     source=inspect.getsource(fn),
                     image=self.attributes["image"],
