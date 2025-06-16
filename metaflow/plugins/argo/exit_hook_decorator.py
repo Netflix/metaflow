64c230fa21fb8dcb3520e8d591b8399ba408b617
Sakari Ikonen <sakari.a.ikonen@gmail.com>
2025-06-16 16:59:25 +0300

    add default kubernetes image. try out script templating

--- metaflow/plugins/argo/exit_hook_decorator.py
+++ metaflow/plugins/argo/exit_hook_decorator.py
@@ -1,7 +1,9 @@
+import platform
 from metaflow.decorators import FlowDecorator
 import inspect
 
 from metaflow.exception import MetaflowException
+from metaflow.metaflow_config import KUBERNETES_CONTAINER_IMAGE
 from metaflow.plugins.argo.exit_hooks import ScriptHook
 
 
@@ -26,12 +28,27 @@ class ExitHookDecorator(FlowDecorator):
                 "Choose at least one of the options on_success/on_error"
             )
 
+        # If no docker image is explicitly specified, impute a default image.
+        if not self.attributes["image"]:
+            # If metaflow-config specifies a docker image, just use that.
+            if KUBERNETES_CONTAINER_IMAGE:
+                self.attributes["image"] = KUBERNETES_CONTAINER_IMAGE
+            # If metaflow-config doesn't specify a docker image, assign a
+            # default docker image.
+            else:
+                # Default to vanilla Python image corresponding to major.minor
+                # version of the Python interpreter launching the flow.
+                self.attributes["image"] = "python:%s.%s" % (
+                    platform.python_version_tuple()[0],
+                    platform.python_version_tuple()[1],
+                )
+
         self.hooks = []
         for success_fn in self.attributes["on_success"]:
             self.hooks.append(
                 ScriptHook(
-                    name=f"success-{success_fn.__name__}",
-                    source=inspect.getsource(success_fn),
+                    name=f"success-{_sanitize_for_argo(success_fn.__name__)}",
+                    source=_wrap_source_in_python_script(success_fn),
                     image=self.attributes["image"],
                     on_success=True,
                 )
@@ -40,9 +57,28 @@ class ExitHookDecorator(FlowDecorator):
         for error_fn in self.attributes["on_error"]:
             self.hooks.append(
                 ScriptHook(
-                    name=f"error-{error_fn.__name__}",
-                    source=inspect.getsource(error_fn),
+                    name=f"error-{_sanitize_for_argo(error_fn.__name__)}",
+                    source=_wrap_source_in_python_script(error_fn),
                     image=self.attributes["image"],
                     on_error=True,
                 )
             )
+
+
+def _sanitize_for_argo(name: str):
+    return name.replace("_", "-")
+
+
+script_template = """
+{src}
+
+if __name__=="__main__":
+    {name}()
+"""
+
+
+def _wrap_source_in_python_script(fn):
+    src = inspect.getsource(fn)
+    name = fn.__name__
+
+    return script_template.format(src=src, name=name)
