c39e54e6a3d1dc24726b978cb874d0f674e6cf70
Romain Cledat <rcledat@netflix.com>
2025-06-26 16:46:47 -0700

    Allow decorators with arguments and decospecs
    Other bug fixes to also make things more natural and just plain
    old stupid bugs

--- test/test_config/hellodecos.py
+++ test/test_config/hellodecos.py
@@ -1,43 +1,68 @@
 from test_included_modules.my_decorators import (
     time_step,
     with_args,
-    MyComplexDecorator,
+    AddArgsDecorator,
+    AddTimeStep,
+    SkipStep,
 )
 
 from hellodecos_base import MyBaseFlowSpec
 
-from metaflow import FlowSpec, step
-from metaflow import config_expr, Config
+from metaflow import step
+from metaflow import Config
 
 
 class DecoFlow(MyBaseFlowSpec):
-
-    cfg = Config("cfg", default_value={"bar": 43})
+    cfg = Config(
+        "cfg",
+        default_value={
+            "args_decorator": "with_args",
+            "user_retry_decorator": "my_decorators.retry",
+            "bar": 43,
+        },
+    )
 
     @step
     def start(self):
         print("Starting flow")
-        self.next(self.middle)
+        print("Added decorators: ", self.user_added_step_decorators)
+        assert self.user_added_step_decorators[0] == "time_step"
+        self.next(self.m0)
 
     @time_step
     @with_args(foo=cfg.bar, bar="baz")
     @step
-    def middle(self):
-        print("Middle step")
-        self.next(self.middle2)
+    def m0(self):
+        print("Added decorators: ", self.user_added_step_decorators)
+        assert self.user_added_step_decorators[0] == "time_step"
+        assert (
+            self.user_added_step_decorators[1] == "with_args({'foo': 43, 'bar': 'baz'})"
+        )
+        print("m0")
+        self.next(self.m1)
 
-    @MyComplexDecorator(skip_steps=["middle2"])
-    # TODO: Does not yet work with middle2 because we need to update the transition
-    # manually. It does skip the step though :)
+    # Shows how a step can be totally skipped
+    @SkipStep(skip_steps=["m1"])
     @step
-    def middle2(self):
-        print("Middle2 step")
+    def m1(self):
+        assert False, "This step should not be executed"
+        self.next(self.m2)
+
+    @AddArgsDecorator(bar=cfg.bar, baz="baz")
+    @AddTimeStep
+    @step
+    def m2(self):
+        print("Added decorators: ", self.user_added_step_decorators)
+        assert (
+            self.user_added_step_decorators[0] == "with_args({'bar': 43, 'baz': 'baz'})"
+        )
+        assert self.user_added_step_decorators[1] == "time_step"
+        print("m2")
         self.next(self.end)
 
-    @MyComplexDecorator(excluded_step_names=["end"])
     @step
     def end(self):
-        print("Ending flow")
+        print("Flow completed successfully")
 
 
 if __name__ == "__main__":
