057d8a6f2b6cc64cd71e48db1f954285f0aa1cf8
Valay Dave <valaygaurang@gmail.com>
2025-07-21 12:38:41 -0700

    [cards] remove imghdr warnings (#2509)
    Remove deprecation warning that resulted in runtime errors.

--- metaflow/_vendor/imghdr/__init__.py
+++ metaflow/_vendor/imghdr/__init__.py
@@ -1,13 +1,8 @@
 """Recognize image file formats based on their first few bytes."""
 
 from os import PathLike
-import warnings
 
 __all__ = ["what"]
-
-
-warnings._deprecated(__name__, remove=(3, 13))
-
 
 #-------------------------#
 # Recognize image headers #
