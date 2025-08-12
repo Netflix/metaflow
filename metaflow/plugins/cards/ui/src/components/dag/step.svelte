5ccf7a32940e843a1b06163c2cf0044f710c772d
Brendan Gibson <brendan@outerbounds.co>
2025-08-12 19:52:57 +0000

    Fix dangling end

--- metaflow/plugins/cards/ui/src/components/dag/step.svelte
+++ metaflow/plugins/cards/ui/src/components/dag/step.svelte
@@ -75,6 +75,7 @@
 
   const currentStep: string = getContext(currentStepContext);
   const current = name === currentStep;
+  step.rendered = true;
 
   // Set color of the step based on the results
   if (step.failed || step.num_failed) {
