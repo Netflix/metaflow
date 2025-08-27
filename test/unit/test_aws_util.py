b91dc5650aa6b3e80402e0d1ad4f5e39f4b99e69
Sakari Ikonen <sakari.a.ikonen@gmail.com>
2025-08-27 14:14:28 +0300

    simplify unit test

--- test/unit/test_aws_util.py
+++ test/unit/test_aws_util.py
@@ -9,17 +9,17 @@ from metaflow.plugins.aws.aws_utils import validate_aws_tag
         ("test", "value", False),
         ("test-with@chars+ - = ._/", "value@with.chars-+ - = ._/", False),
         (
-            "not-too-long-key-value-" + "a" * 105,
+            "a" * 128,
             "ok",
             False,
         ),  # <=128 char key should work.
-        ("too-long-key-value-" + "a" * 110, "ok", True),  # >128 char key should fail.
+        ("a" * 129, "ok", True),  # >128 char key should fail.
         (
             "ok",
-            "not-too-long-value-" + "a" * 237,
+            "a" * 256,
             False,
         ),  # <=256 char value should work.
-        ("ok", "too-long-value-" + "a" * 242, True),  # >256 char value should fail.
+        ("ok", "a" * 257, True),  # >256 char value should fail.
         ("aWs:not-allowed", "ok", True),  # 'aws:' prefix should not be allowed as key
         ("ok", "AWS:not-allowed", True),  # 'aws:' prefix should not be allowed as value
         (
