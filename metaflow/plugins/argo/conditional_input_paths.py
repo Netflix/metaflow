ebc35bf4d73f5f1ef9a9c5ebe39277f9e4f9a6e3
Sakari Ikonen <sakari.a.ikonen@gmail.com>
2025-12-01 18:40:53 +0200

    fix for conditional input-paths when multiple leading conditinals might exist

--- metaflow/plugins/argo/conditional_input_paths.py
+++ metaflow/plugins/argo/conditional_input_paths.py
@@ -20,12 +20,15 @@ def generate_input_paths(input_paths, skippable_steps):
     # all pathspecs of leading steps that executed.
     trimmed = [path for path in paths if not "{{" in path]
 
+    # If the input-path is from a conditional, we want to pick the one that is last-in-line in the DAG.
+    # The order of graph parsing ensures that the steps are in reverse order of occurence, so the first one is the latest.
+    latest_conditional_in_graph = trimmed[:1]
     # pathspecs of leading steps that are conditional, and should be used instead of non-conditional ones
     # e.g. the case of skipping switches: start -> case_step -> conditional_a or end
     conditionals = [
         path for path in trimmed if not any(step in path for step in skippable_steps)
     ]
-    pathspecs_to_use = conditionals if conditionals else trimmed
+    pathspecs_to_use = conditionals if conditionals else latest_conditional_in_graph
     return compress_list(pathspecs_to_use, zlibmin=inf)
 
 
