70d5d713ffd255725e7daa4cfecd1e5f3c6cc5c9
Sakari Ikonen <sakari.a.ikonen@gmail.com>
2026-01-21 00:33:19 +0200

    cleanup

--- metaflow/plugins/argo/set_parameters.py
+++ metaflow/plugins/argo/set_parameters.py
@@ -9,10 +9,13 @@ def parse_parameter_value(base64_value):
     val = base64.b64decode(base64_value).decode("utf-8")
 
     try:
-        return json.loads(val)
+        res = json.loads(val)
     except json.decoder.JSONDecodeError:
         # fallback to using the original value.
-        return val
+        res = val
+
+    # (re)serialize for storing in an environment variable
+    return json.dumps(res)
 
 
 def export_parameters(output_file: str, params: Dict[str, str]) -> None:
@@ -25,7 +28,7 @@ def export_parameters(output_file: str, params: Dict[str, str]) -> None:
                 "export METAFLOW_INIT_%s=%s\n"
                 % (
                     k.upper().replace("-", "_"),
-                    json.dumps(parse_parameter_value(params[k])),
+                    parse_parameter_value(params[k]),
                 )
             )
     os.chmod(output_file, 509)
