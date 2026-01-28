cca651e96afee3fb07d4819d5bc01101e9b08483
tfurmston <tfurmston@users.noreply.github.com>
2026-01-28 20:24:24 +0000

    remove stray import

--- metaflow/plugins/argo/argo_workflows.py
+++ metaflow/plugins/argo/argo_workflows.py
@@ -52,7 +52,6 @@ from metaflow.metaflow_config import (
 from metaflow.metaflow_config_funcs import config_values
 from metaflow.mflog import BASH_SAVE_LOGS, bash_capture_logs, export_mflog_env_vars
 from metaflow.parameters import deploy_time_eval
-
 from metaflow.plugins.kubernetes.kube_utils import qos_requests_and_limits
 
 from metaflow.plugins.kubernetes.kubernetes_jobsets import KubernetesArgoJobSet
