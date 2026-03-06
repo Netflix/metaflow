d3cd3a4608415c353bfa92ce99860f58484fad0a
Nissan Pow <npow@netflix.com>
2026-03-06 12:45:52 -0800

    Revert inputs.py change: fix in flow_datastore.py is sufficient

--- metaflow/datastore/inputs.py
+++ metaflow/datastore/inputs.py
@@ -6,16 +6,7 @@ class Inputs(object):
     """
 
     def __init__(self, flows):
-        # Sort by foreach index so that inputs arrive in the same order as the
-        # original foreach list regardless of task completion order or datastore
-        # retrieval order.
-        def _foreach_index(flow):
-            stack = getattr(flow, "_foreach_stack", None)
-            if stack:
-                return stack[-1].index
-            return 0
-
-        self.flows = sorted(flows, key=_foreach_index)
+        self.flows = list(flows)
         for flow in self.flows:
             setattr(self, flow._current_step, flow)
 
