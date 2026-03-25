7cb13c91364df44bf78cff5aaf25b145fa37eb0c
Nissan Pow <npow@netflix.com>
2026-03-24 17:33:58 -0700

    fix: revert localbatch version guard now that localbatch 0.4.0 supports Python 3.7

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ setup(
             "pytest-rerunfailures",
             "omegaconf",
             "kubernetes",
-            'localbatch; python_version >= "3.8"',
+            "localbatch",
         ],
     },
 )
